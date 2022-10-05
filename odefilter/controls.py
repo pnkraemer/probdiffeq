211ab0bace2b180d7d09e958620be39a72c38733
Nicholas Krämer <nicokraemer@gmx.net>
2022-10-05 12:26:33 +0200

    More controls

--- odefilter/controls.py
+++ odefilter/controls.py
@@ -1,5 +1,6 @@
 """Step-size selection."""
 
+import abc
 from functools import partial
 from typing import NamedTuple
 
@@ -7,12 +8,22 @@ import jax
 import jax.numpy as jnp
 
 
+class AbstractControl(abc.ABC):
+    @abc.abstractmethod
+    def init_fn(self):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def control_fn(self, state, error_normalised, error_order, params):
+        raise NotImplementedError
+
+
 def proportional_integral():
     """Proportional-integral control."""
     return _PIControl(), _PIControl.Params()
 
 
-class _PIControl:
+class _PIControl(AbstractControl):
     class Params(NamedTuple):
         safety = 0.95
         factor_min = 0.2
@@ -27,8 +38,8 @@ class _PIControl:
     def init_fn(self):
         return self.State(scale_factor=1.0, error_norm_previously_accepted=1.0)
 
-    def step_fn(self, *, state, error_normalised, error_order, params):
-        scale_factor = _scale_factor_proportional_integral(
+    def control_fn(self, *, state, error_normalised, error_order, params):
+        scale_factor = self._scale_factor_proportional_integral(
             error_norm=error_normalised,
             error_order=error_order,
             error_norm_previously_accepted=state.error_norm_previously_accepted,
@@ -48,79 +59,73 @@ class _PIControl:
             error_norm_previously_accepted=error_norm_previously_accepted,
         )
 
+    @staticmethod
+    def _scale_factor_proportional_integral(
+        *,
+        error_norm,
+        error_norm_previously_accepted,
+        error_order,
+        safety,
+        factor_min,
+        factor_max,
+        power_integral_unscaled,
+        power_proportional_unscaled,
+    ):
+        """Proportional-integral control.
 
-@partial(jax.jit, static_argnames=("f",))
-def _propose_first_dt(*, f, u0):
-    norm_y0 = jnp.linalg.norm(u0)
-    norm_dy0 = jnp.linalg.norm(f(u0))
-    return 0.01 * norm_y0 / norm_dy0
+        Proportional-integral control simplifies to integral control
+        when the parameters are chosen as
+
+            `power_integral_unscaled=1`,
+            `power_proportional_unscaled=0`.
+        """
+        n1 = power_integral_unscaled / error_order
+        n2 = power_proportional_unscaled / error_order
+
+        a1 = (1.0 / error_norm) ** n1
+        a2 = (error_norm_previously_accepted / error_norm) ** n2
+        scale_factor = safety * a1 * a2
+
+        scale_factor_clipped = jnp.maximum(
+            factor_min, jnp.minimum(scale_factor, factor_max)
+        )
+        return scale_factor_clipped
 
 
-@partial(jax.jit, static_argnames=("f",))
-def _propose_first_dt_per_tol(*, f, u0, num_derivatives, rtol, atol):
-    # Taken from:
-    # https://github.com/google/jax/blob/main/jax/experimental/ode.py
-    #
-    # which uses the algorithm from
-    #
-    # E. Hairer, S. P. Norsett G. Wanner,
-    # Solving Ordinary Differential Equations I: Nonstiff Problems, Sec. II.4.
-    f0 = f(u0)
-    scale = atol + u0 * rtol
-    a = jnp.linalg.norm(u0 / scale)
-    b = jnp.linalg.norm(f0 / scale)
-    dt0 = jnp.where((a < 1e-5) | (b < 1e-5), 1e-6, 0.01 * a / b)
-
-    u1 = u0 + dt0 * f0
-    f1 = f(u1)
-    c = jnp.linalg.norm((f1 - f0) / scale) / dt0
-    dt1 = jnp.where(
-        (b <= 1e-15) & (c <= 1e-15),
-        jnp.maximum(1e-6, dt0 * 1e-3),
-        (0.01 / jnp.max(b + c)) ** (1.0 / (num_derivatives + 1)),
-    )
-    return jnp.minimum(100.0 * dt0, dt1)
-
-
-@jax.jit
-def _scale_factor_integral_control(
-    *, error_norm, safety, error_order, factor_min, factor_max
-):
+def integral():
     """Integral control."""
-    scale_factor = safety * (error_norm ** (-1.0 / error_order))
-    scale_factor_clipped = jnp.maximum(
-        factor_min, jnp.minimum(scale_factor, factor_max)
-    )
-    return scale_factor_clipped
+    return _IControl(), _IControl.Params()
 
 
-def _scale_factor_proportional_integral(
-    *,
-    error_norm,
-    error_norm_previously_accepted,
-    error_order,
-    safety,
-    factor_min,
-    factor_max,
-    power_integral_unscaled,
-    power_proportional_unscaled,
-):
-    """Proportional-integral control.
+class _IControl(AbstractControl):
+    class State(NamedTuple):
+        scale_factor: float
 
-    Proportional-integral control simplifies to integral control
-    when the parameters are chosen as
+    class Params(NamedTuple):
+        safety = 0.95
+        factor_min = 0.2
+        factor_max = 10.0
 
-        `power_integral_unscaled=1`,
-        `power_proportional_unscaled=0`.
-    """
-    n1 = power_integral_unscaled / error_order
-    n2 = power_proportional_unscaled / error_order
+    def init_fn(self):
+        return self.State(scale_factor=1.0)
 
-    a1 = (1.0 / error_norm) ** n1
-    a2 = (error_norm_previously_accepted / error_norm) ** n2
-    scale_factor = safety * a1 * a2
+    def control_fn(self, state, error_normalised, error_order, params):
+        scale_factor = self._scale_factor_integral_control(
+            error_norm=error_normalised,
+            error_order=error_order,
+            safety=params.safety,
+            factor_min=params.factor_min,
+            factor_max=params.factor_max,
+        )
+        return self.State(scale_factor=scale_factor)
 
-    scale_factor_clipped = jnp.maximum(
-        factor_min, jnp.minimum(scale_factor, factor_max)
-    )
-    return scale_factor_clipped
+    @staticmethod
+    def _scale_factor_integral_control(
+        *, error_norm, safety, error_order, factor_min, factor_max
+    ):
+        """Integral control."""
+        scale_factor = safety * (error_norm ** (-1.0 / error_order))
+        scale_factor_clipped = jnp.maximum(
+            factor_min, jnp.minimum(scale_factor, factor_max)
+        )
+        return scale_factor_clipped
--- tests/test_controls.py
+++ tests/test_controls.py
@@ -11,15 +11,23 @@ def case_proportional_integral():
     return controls.proportional_integral()
 
 
+@pytest_cases.case
+def case_integral():
+    return controls.integral()
+
+
 @pytest_cases.parametrize_with_cases("control", cases=".")
 def test_control(control):
     alg, params = control
+
+    assert isinstance(alg, controls.AbstractControl)
+
     state = alg.init_fn()
     assert state.scale_factor > 0.0
 
     error_normalised = 10.0
     error_order = 3
-    state = alg.step_fn(
+    state = alg.control_fn(
         state=state,
         error_normalised=error_normalised,
         error_order=error_order,
