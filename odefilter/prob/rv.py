69d974617ca481b1b68f715bf815792b0b9b173d
Nicholas Krämer <nicokraemer@gmx.net>
2022-10-07 19:00:51 +0200

    remove separation

--- odefilter/prob/rv.py
+++ odefilter/prob/rv.py
@@ -3,6 +3,7 @@
 from typing import Any
 
 import equinox as eqx
+from jaxtyping import Array, Float
 
 
 class Normal(eqx.Module):
@@ -10,3 +11,10 @@ class Normal(eqx.Module):
 
     mean: Any
     cov_sqrtm_upper: Any
+
+
+class IsotropicNormal(eqx.Module):
+    """Random variable with a normal distribution."""
+
+    mean: Float[Array, "n d"]
+    cov_sqrtm_upper: Float[Array, "n n"]
