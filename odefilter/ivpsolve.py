114b190b7ebe75f88d794713e940dcfc147b0f03
Nicholas Krämer <nicokraemer@gmx.net>
2022-10-09 09:35:03 +0200

    Solve for checkpoints [draft]

--- odefilter/ivpsolve.py
+++ odefilter/ivpsolve.py
@@ -68,6 +68,41 @@ def simulate_terminal_values(
     return state.accepted
 
 
+def solve_checkpoints(vector_field, initial_values, *, ts, solver, parameters=()):
+    initial_value_is_not_scalar = jax.tree_util.tree_map(
+        lambda x: jnp.ndim(x) > 0, initial_values
+    )
+    assert jax.tree_util.tree_all(initial_value_is_not_scalar)
+
+    # Include the parameters into the vector field.
+    # This is done inside this function, because we don't want to
+    # re-compile the whole solve if a parameter changes.
+    def vf(*ys, t):
+        return vector_field(*ys, t, *parameters)
+
+    def solve_for_next_checkpoint(s, t_next):
+        state_ = _advance_ivp_solution_adaptively(
+            state0=s,
+            t1=t_next,
+            vector_field=vf,
+            step_fn=solver.step_fn,
+        )
+        return state_, state_.accepted
+
+    state0 = solver.init_fn(vector_field=vf, initial_values=initial_values, t0=ts[0])
+    _, solution = jax.lax.scan(
+        f=solve_for_next_checkpoint,
+        init=state0,
+        xs=ts[1:],
+        reverse=False,
+    )
+    return solution
+
+    # Return the initial value along with the rest, because
+    # it contains components of the filtering solution
+    return extract_qoi_fn(*state_terminal), state_qoi, extract_qoi_fn(*init_val)
+
+
 def _advance_ivp_solution_adaptively(*, vector_field, t1, state0, step_fn):
     """Advance an IVP solution from an initial state to a terminal state."""
 
--- tests/test_ivpsolve.py
+++ tests/test_ivpsolve.py
@@ -40,3 +40,21 @@ def test_simulate_terminal_values(vf, u0, t0, t1, p, solver):
 
     assert solution.t == t1
     assert jnp.allclose(solution.u, 1.0, atol=1e-1, rtol=1e-1)
+
+
+@pytest_cases.parametrize_with_cases("vf, u0, t0, t1, p", cases=".cases_problems")
+@pytest_cases.parametrize_with_cases("solver", cases=".")
+def test_solve_checkpoints(vf, u0, t0, t1, p, solver):
+    ts = jnp.linspace(t0, t1, num=10)
+    solution = ivpsolve.solve_checkpoints(
+        vector_field=vf,
+        initial_values=u0,
+        ts=ts,
+        parameters=p,
+        solver=solver,
+    )
+
+    assert jnp.allclose(solution.t, ts[1:])
+    assert jnp.allclose(solution.u[-1], 1.0, atol=1e-1, rtol=1e-1)
+    print(solution)
+    assert False
