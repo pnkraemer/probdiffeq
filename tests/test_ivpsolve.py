789459c133fb4412cedee78b51b1bb0ed23f82c0
Nicholas Krämer <nicokraemer@gmx.net>
2022-10-09 11:57:47 +0200

    Solver tests corrected

--- tests/test_ivpsolve.py
+++ tests/test_ivpsolve.py
@@ -33,8 +33,9 @@ def case_odefilter(taylor, backend):
 @pytest_cases.parametrize_with_cases("vf, u0, t0, t1, p", cases=".cases_problems")
 @pytest_cases.parametrize_with_cases("solver", cases=".")
 def test_simulate_terminal_values(vf, u0, t0, t1, p, solver):
-    odeint_sol = odeint(vf, u0[0], jnp.asarray([t0, t1]), *p, atol=1e-6, rtol=1e-6)
-    ys_reference = odeint_sol[-1, :]
+    odeint_solution = odeint(vf, u0[0], jnp.asarray([t0, t1]), *p, atol=1e-6, rtol=1e-6)
+    ys_reference = odeint_solution[-1, :]
+
     solution = ivpsolve.simulate_terminal_values(
         vector_field=vf,
         initial_values=u0,
@@ -52,6 +53,10 @@ def test_simulate_terminal_values(vf, u0, t0, t1, p, solver):
 @pytest_cases.parametrize_with_cases("solver", cases=".")
 def test_solve_checkpoints(vf, u0, t0, t1, p, solver):
     ts = jnp.linspace(t0, t1, num=10)
+
+    odeint_solution = odeint(vf, u0[0], ts, *p, atol=1e-6, rtol=1e-6)
+    ts_reference, ys_reference = ts[1:], odeint_solution[1:, :]
+
     solution = ivpsolve.solve_checkpoints(
         vector_field=vf,
         initial_values=u0,
@@ -59,5 +64,5 @@ def test_solve_checkpoints(vf, u0, t0, t1, p, solver):
         parameters=p,
         solver=solver,
     )
-    assert jnp.allclose(solution.t, ts[1:])
-    assert jnp.allclose(solution.u[-1], 1.0, atol=1e-1, rtol=1e-1)
+    assert jnp.allclose(solution.t, ts_reference)
+    assert jnp.allclose(solution.u, ys_reference, atol=1e-3, rtol=1e-3)
