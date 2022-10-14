228189124d30d4bf5dc0d3da3a77fff48a97a5f4
Nicholas Krämer <nicokraemer@gmx.net>
2022-10-14 09:25:20 +0200

    Failing unit test for smoother equivalence

--- tests/ivp_cases.py
+++ tests/ivp_cases.py
@@ -7,10 +7,11 @@ from pytest_cases import case
 
 @case
 def problem_lotka():
-    f, u0, tspan, f_args = ivps.lotka_volterra()
+    f, u0, (t0, t1), f_args = ivps.lotka_volterra()
 
     @jax.jit
     def vf(_t, x, *p):
         return f(x, *p)
 
-    return vf, (u0,), *tspan, f_args
+    # Only very short time-intervals are sufficient for a unit test.
+    return vf, (u0,), t0, 0.25 * t1, f_args
--- tests/test_smoothing_equivalences.py
+++ tests/test_smoothing_equivalences.py
@@ -1,5 +1,6 @@
 """There are too many ways to smooth. We assert they all do the same."""
 
+import jax
 import jax.numpy as jnp
 from jax.tree_util import tree_all, tree_map
 from pytest_cases import case, parametrize, parametrize_with_cases
@@ -45,6 +46,17 @@ def test_final_state_equal_to_filter(vf, u0, t0, t1, p, ekf, eks):
     assert _tree_all_allclose(ekf_sol.diffusion_sqrtm, eks_sol.diffusion_sqrtm)
 
 
+@case
+@parametrize("n", [1, 3])
+@parametrize("tol", [1e-1, 1e-3])
+def smoother_fixpt_smoother_pair_eks0(n, tol):
+    eks0 = recipes.dynamic_isotropic_eks0(num_derivatives=n, atol=1e-2 * tol, rtol=tol)
+    fixpt_eks0 = recipes.dynamic_isotropic_fixpt_eks0(
+        num_derivatives=n, atol=1e-2 * tol, rtol=tol
+    )
+    return eks0, fixpt_eks0
+
+
 @parametrize_with_cases("vf, u0, t0, t1, p", cases=".ivp_cases", prefix="problem_")
 @parametrize_with_cases(
     "eks, fixpt_eks", cases=".", prefix="smoother_fixpt_smoother_pair_"
@@ -53,7 +65,29 @@ def test_smoothing_checkpoint_equals_solver_state(vf, u0, t0, t1, p, eks, fixpt_
     """In simulate_checkpoints(), if the checkpoint-grid equals the solution-grid
     of a previous call to solve(), the results should be identical."""
 
-    assert False
+    eks_sol = ivpsolve.solve(
+        vf, u0, t0=t0, t1=t1, parameters=p, solver=eks[0], info_op=eks[1]
+    )
+    with jax.disable_jit():
+        fixpt_eks_sol = ivpsolve.simulate_checkpoints(
+            vf,
+            u0,
+            ts=eks_sol.t,
+            parameters=p,
+            solver=fixpt_eks[0],
+            info_op=fixpt_eks[1],
+        )
+    allclose = _tree_allclose(fixpt_eks_sol, eks_sol)
+
+    print(allclose.t)
+    print(allclose.u)
+    print(allclose.filtered.mean)
+    print(allclose.filtered.cov_sqrtm_lower)
+    print(allclose.backward_model.transition)
+    print(allclose.backward_model.noise.mean)
+    print(allclose.backward_model.noise.cov_sqrtm_lower)
+
+    assert _tree_all_allclose(fixpt_eks_sol, eks_sol)
 
 
 def test_smoothing_coarser_checkpoints():
@@ -65,7 +99,12 @@ def test_smoothing_finer_checkpoints():
 
 
 def _tree_all_allclose(tree1, tree2, **kwargs):
+    trees_is_allclose = _tree_allclose(tree1, tree2, **kwargs)
+    return tree_all(trees_is_allclose)
+
+
+def _tree_allclose(tree1, tree2, **kwargs):
     def allclose_partial(*args):
         return jnp.allclose(*args, **kwargs)
 
-    return tree_all(tree_map(allclose_partial, tree1, tree2))
+    return tree_map(allclose_partial, tree1, tree2)
