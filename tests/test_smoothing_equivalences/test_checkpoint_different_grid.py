be92aec97865ceaff0d6648ecff4e5f36a770c90
Nicholas Krämer <nicokraemer@gmx.net>
2022-10-16 18:04:49 +0200

    Skipper

--- tests/test_smoothing_equivalences/test_checkpoint_different_grid.py
+++ tests/test_smoothing_equivalences/test_checkpoint_different_grid.py
@@ -27,40 +27,40 @@ def smoother_fixpt_smoother_pair_eks0(n, tol):
 @parametrize_with_cases(
     "eks, fixpt_eks", cases=".", prefix="smoother_fixpt_smoother_pair_"
 )
-def test_smoothing_checkpoint_equals_solver_state(vf, u0, t0, t1, p, eks, fixpt_eks):
+def test_smoothing_checkpoint_equals_solver_state_smaller_grid(
+    vf, u0, t0, t1, p, eks, fixpt_eks
+):
     """In simulate_checkpoints(), if the checkpoint-grid equals the solution-grid\
      of a previous call to solve(), the results should be identical."""
-    eks_sol = ivpsolve.solve(
-        vf, u0, t0=t0, t1=t1, parameters=p, solver=eks[0], info_op=eks[1]
-    )
-
     # eks_sol.t is an adaptive grid
     # here, create an even grid which shares one point with the adaptive one.
     # This one point will be used for error-estimation.
-    ts, t = _grid(eks_sol.t, t0=t0, t1=t1, factor=3)
-    fixpt_eks_sol = ivpsolve.simulate_checkpoints(
-        vf,
-        u0,
-        ts=ts,
-        parameters=p,
-        solver=fixpt_eks[0],
-        info_op=fixpt_eks[1],
+    eks_sol = ivpsolve.solve(
+        vf, u0, t0=t0, t1=t1, parameters=p, solver=eks[0], info_op=eks[1]
     )
+    with jax.disable_jit():
+        ts, t = _grid(eks_sol.t, t0=t0, t1=t1, factor=5)
+        print("From here one......")
+        fixpt_eks_sol = ivpsolve.simulate_checkpoints(
+            vf,
+            u0,
+            ts=jnp.linspace(t0, t1, num=35),
+            # ts=jnp.linspace(t0, t1, num=len(eks_sol.t) * 3),
+            # ts=ts,
+            # ts=jnp.asarray([t0,t-0.01, t, t1- 0.01]),
+            parameters=p,
+            solver=fixpt_eks[0],
+            info_op=fixpt_eks[1],
+        )
     assert t in ts
     assert t in eks_sol.t
-
-    idx_eks, *_ = jnp.where(eks_sol.t[:, None] == jnp.asarray([t0, t, t1]))
-    idx_fixpt, *_ = jnp.where(fixpt_eks_sol.t[:, None] == jnp.asarray([t0, t, t1]))
-
-    print(fixpt_eks_sol.u[idx_fixpt], eks_sol.u[idx_eks])
-    assert jnp.allclose(fixpt_eks_sol.t[idx_fixpt], eks_sol.t[idx_eks])
-    assert jnp.allclose(fixpt_eks_sol.u[idx_fixpt], eks_sol.u[idx_eks])
+    # print(fixpt_eks_sol.t_previous)
 
     import matplotlib.pyplot as plt
 
     plt.plot(
         eks_sol.t,
-        eks_sol.filtered.mean[:, -1, :],
+        eks_sol.filtered.mean[:, :, -1],
         linestyle="None",
         marker="X",
         markersize=8,
@@ -70,17 +70,26 @@ def test_smoothing_checkpoint_equals_solver_state(vf, u0, t0, t1, p, eks, fixpt_
     plt.axvline(t)
     plt.plot(
         fixpt_eks_sol.t,
-        fixpt_eks_sol.filtered.mean[:, -1, :],
+        fixpt_eks_sol.filtered.mean[:, :, -1],
         # linestyle="None",
         linewidth=0.1,
-        marker="P",
+        marker="o",
         markersize=12,
         alpha=0.5,
         label="FixPtEKS",
     )
+    # plt.ylim((-8, -2))
     plt.legend()
-    plt.ylim((-15, 15))
     plt.show()
+
+    idx_eks, *_ = jnp.where(eks_sol.t[:, None] == jnp.asarray([t0, t, t1]))
+    idx_fixpt, *_ = jnp.where(fixpt_eks_sol.t[:, None] == jnp.asarray([t0, t, t1]))
+
+    print(fixpt_eks_sol.u[idx_fixpt], eks_sol.u[idx_eks])
+    assert jnp.allclose(fixpt_eks_sol.t[idx_fixpt], eks_sol.t[idx_eks])
+    assert jnp.allclose(fixpt_eks_sol.u[idx_fixpt], eks_sol.u[idx_eks])
+
+    assert False
 
 
 # jit to reduce potential floating-point inconsistencies
