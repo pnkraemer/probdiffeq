1f31fd4c9867e7c7e15b34cc0fd6979ea59ced5f
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-10-17 18:19:34 +0200

    Remove comments

--- tests/test_tornadox_equivalence.py
+++ tests/test_tornadox_equivalence.py
@@ -112,51 +112,6 @@ def case_solver_pair_dynamic_ek1(num, atol, rtol, factor_min, factor_max, safety
     "solution_tornadox, solution_odefilter", cases=".", prefix="case_solver_pair_"
 )
 def test_outputs_equal(solution_tornadox, solution_odefilter):
-    #
-    # # van-der-Pol as a setup. We really don't want stiffness here.
-    # f, u0, (t0, t1), f_args = ivps.van_der_pol_first_order(
-    #     time_span=(0.0, 1.0), stiffness_constant=1.0
-    # )
-    #
-    # @jax.jit
-    # def vf(t, y):
-    #     return f(y, *f_args)
-    #
-    # num = 4  # this is the default argument
-    # # stricter than default, because we want to reject steps!
-    # atol, rtol = (1e-4, 1e-6)
-    # factor_min, factor_max, safety = 0.2, 10.0, 0.95  # default
-    #
-    # # Tornadox. These arguments are defaults, but we fix them anyway.
-    # solver = ek0.KroneckerEK0(
-    #     initialization=init.TaylorMode(),
-    #     num_derivatives=num,
-    #     steprule=step.AdaptiveSteps(
-    #         max_changes=(factor_min, factor_max),
-    #         safety_scale=safety,
-    #         abstol=atol,
-    #         reltol=rtol,
-    #     ),
-    # )
-    # vdp = ivp.InitialValueProblem(f=vf, t0=t0, tmax=t1, y0=u0)
-    # solution_tornadox = solver.solve(vdp)
-    #
-    # # ODE-filter. We need to use a very specific setup.
-    # ekf0, info_op = recipes.dynamic_isotropic_ekf0(num_derivatives=num)
-    # controller = controls.ClippedIntegral(
-    #     safety=safety, factor_min=factor_min, factor_max=factor_max
-    # )
-    # solution_odefilter = ivpsolve.solve(
-    #     vf,
-    #     initial_values=(u0,),
-    #     t0=t0,
-    #     t1=t1,
-    #     solver=ekf0,
-    #     info_op=info_op,
-    #     atol=atol,
-    #     rtol=rtol,
-    #     control=controller,
-    # )
 
     # Compare t, mean, cov_sqrtm @ cov_sqrtm.T.
     # They should be _identical_ (up to machine precision).
