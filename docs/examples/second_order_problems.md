e6955b427bce97c91214f6211ed945c3fe1048b5
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-10-18 15:22:24 +0200

    Add an example notebook for second-order problems

---
+++ docs/examples/second_order_problems.md
@@ -0,0 +1,102 @@
+---
+jupyter:
+  jupytext:
+    formats: ipynb,md
+    text_representation:
+      extension: .md
+      format_name: markdown
+      format_version: '1.3'
+      jupytext_version: 1.14.1
+  kernelspec:
+    display_name: Python 3 (ipykernel)
+    language: python
+    name: python3
+---
+
+# Simulate second-order systems
+
+```python
+import jax
+import jax.numpy as jnp
+import matplotlib.pyplot as plt
+from diffeqzoo import backend, ivps
+from jax.config import config
+
+from odefilter import ivpsolve, recipes
+
+config.update("jax_enable_x64", True)
+backend.select("jax")
+```
+
+Quick refresher: first-order ODEs
+
+```python
+f, u0, (t0, t1), f_args = ivps.three_body_restricted_first_order()
+
+
+@jax.jit
+def vf_1(y, t, p):
+    return f(y, *p)
+
+
+ek0_1, info_op_1 = recipes.dynamic_isotropic_ekf0(num_derivatives=5)
+ts = jnp.linspace(t0, t1, endpoint=True, num=500)
+```
+
+```python
+%%timeit
+
+solution = ivpsolve.simulate_checkpoints(
+    vf_1,
+    initial_values=(u0,),
+    ts=ts,
+    solver=ek0_1,
+    info_op=info_op_1,
+    atol=1e-5,
+    rtol=1e-5,
+    parameters=f_args,
+)
+```
+
+```python
+plt.title((solution.u.shape, jnp.linalg.norm(solution.u[-1, ...] - u0)))
+plt.plot(solution.u[:, 0], solution.u[:, 1])
+plt.show()
+```
+
+```python
+f, (u0, du0), (t0, t1), f_args = ivps.three_body_restricted()
+
+
+@jax.jit
+def vf_2(y, dy, t, p):
+    return f(y, dy, *p)
+
+
+# One derivative more than above because we don't transform to first order
+ek0_2, info_op_2 = recipes.dynamic_isotropic_ekf0(num_derivatives=6, ode_order=2)
+ts = jnp.linspace(t0, t1, endpoint=True, num=500)
+```
+
+```python
+%%timeit
+
+solution = ivpsolve.simulate_checkpoints(
+    vf_2,
+    initial_values=(u0, du0),
+    ts=ts,
+    solver=ek0_2,
+    info_op=info_op_2,
+    atol=1e-5,
+    rtol=1e-5,
+    parameters=f_args,
+)
+```
+
+```python
+plt.title((solution.u.shape, jnp.linalg.norm(solution.u[-1, ...] - u0)))
+plt.plot(solution.u[:, 0], solution.u[:, 1])
+plt.show()
+```
+
+The second-order solver is faster and way more accurate.
--- odefilter/recipes.py
+++ odefilter/recipes.py
@@ -13,83 +13,105 @@ from odefilter.implementations import dense, isotropic
 from odefilter.strategies import filters, smoothers
 
 
-def dynamic_isotropic_ekf0(*, num_derivatives=4):
+def dynamic_isotropic_ekf0(*, num_derivatives=4, ode_order=1):
     """Construct the equivalent of an explicit solver with an isotropic covariance \
     structure, dynamic calibration, and optimised for terminal-value simulation.
 
     Suitable for high-dimensional, non-stiff problems.
     """
+    _assert_num_derivatives_sufficiently_large(
+        num_derivatives=num_derivatives, ode_order=ode_order
+    )
     implementation = isotropic.IsotropicImplementation.from_num_derivatives(
         num_derivatives=num_derivatives
     )
     solver = filters.DynamicFilter(implementation=implementation)
-    information_op = information.isotropic_ek0(ode_order=1)
+    information_op = information.isotropic_ek0(ode_order=ode_order)
     return solver, information_op
 
 
-def dynamic_isotropic_eks0(*, num_derivatives=4):
+def dynamic_isotropic_eks0(*, num_derivatives=4, ode_order=1):
     """Construct the equivalent of an explicit solver with an isotropic covariance \
     structure and dynamic calibration.
 
     Suitable for high-dimensional, non-stiff problems.
     """
+    _assert_num_derivatives_sufficiently_large(
+        num_derivatives=num_derivatives, ode_order=ode_order
+    )
     implementation = isotropic.IsotropicImplementation.from_num_derivatives(
         num_derivatives=num_derivatives
     )
     solver = smoothers.DynamicSmoother(implementation=implementation)
-    information_op = information.isotropic_ek0(ode_order=1)
+    information_op = information.isotropic_ek0(ode_order=ode_order)
     return solver, information_op
 
 
-def dynamic_isotropic_fixpt_eks0(*, num_derivatives=4):
+def dynamic_isotropic_fixpt_eks0(*, num_derivatives=4, ode_order=1):
     """Construct the equivalent of an explicit solver with an isotropic covariance \
     structure and dynamic calibration.
 
     Suitable for high-dimensional, non-stiff problems.
     """
+    _assert_num_derivatives_sufficiently_large(
+        num_derivatives=num_derivatives, ode_order=ode_order
+    )
     implementation = isotropic.IsotropicImplementation.from_num_derivatives(
         num_derivatives=num_derivatives
     )
     solver = smoothers.DynamicFixedPointSmoother(implementation=implementation)
-    information_op = information.isotropic_ek0(ode_order=1)
+    information_op = information.isotropic_ek0(ode_order=ode_order)
     return solver, information_op
 
 
-def dynamic_ekf1(*, ode_dimension, num_derivatives=4):
+def dynamic_ekf1(*, ode_dimension, num_derivatives=4, ode_order=1):
     """Construct the equivalent of a semi-implicit solver with dynamic calibration.
 
     Suitable for low-dimensional, stiff problems.
     """
+    _assert_num_derivatives_sufficiently_large(
+        num_derivatives=num_derivatives, ode_order=ode_order
+    )
     implementation = dense.DenseImplementation.from_num_derivatives(
         num_derivatives=num_derivatives, ode_dimension=ode_dimension
     )
     solver = filters.DynamicFilter(implementation=implementation)
-    information_op = information.ek1(ode_dimension=ode_dimension, ode_order=1)
+    information_op = information.ek1(ode_dimension=ode_dimension, ode_order=ode_order)
     return solver, information_op
 
 
-def ekf1(*, ode_dimension, num_derivatives=4):
+def ekf1(*, ode_dimension, num_derivatives=4, ode_order=1):
     """Construct the equivalent of a semi-implicit solver.
 
     Suitable for low-dimensional, stiff problems.
     """
+    _assert_num_derivatives_sufficiently_large(
+        num_derivatives=num_derivatives, ode_order=ode_order
+    )
     implementation = dense.DenseImplementation.from_num_derivatives(
         num_derivatives=num_derivatives, ode_dimension=ode_dimension
     )
     solver = filters.Filter(implementation=implementation)
-    information_op = information.ek1(ode_dimension=ode_dimension, ode_order=1)
+    information_op = information.ek1(ode_dimension=ode_dimension, ode_order=ode_order)
     return solver, information_op
 
 
-def isotropic_ekf0(*, num_derivatives=4):
+def isotropic_ekf0(*, num_derivatives=4, ode_order=1):
     """Construct the equivalent of an explicit solver with an isotropic covariance \
     structure, and optimised for terminal-value simulation.
 
     Suitable for high-dimensional, non-stiff problems.
     """
+    _assert_num_derivatives_sufficiently_large(
+        num_derivatives=num_derivatives, ode_order=ode_order
+    )
     implementation = isotropic.IsotropicImplementation.from_num_derivatives(
         num_derivatives=num_derivatives
     )
     solver = filters.Filter(implementation=implementation)
-    information_op = information.isotropic_ek0(ode_order=1)
+    information_op = information.isotropic_ek0(ode_order=ode_order)
     return solver, information_op
+
+
+def _assert_num_derivatives_sufficiently_large(*, num_derivatives, ode_order):
+    assert num_derivatives >= ode_order
