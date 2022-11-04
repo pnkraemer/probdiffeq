c13139928d891ded4daff0f036666a019a8f4d68
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-11-04 14:32:58 +0100

    Test filter

--- tests/test_solve_fixed_grid.py
+++ tests/test_solve_fixed_grid.py
@@ -1,6 +1,13 @@
 """Tests for IVP solvers."""
+import functools
 
+import jax
 import jax.numpy as jnp
+import jax.tree_util
+
+from odefilter import ivpsolve, solvers
+from odefilter.implementations import isotropic
+from odefilter.strategies import filters, smoothers
 
 
 def test_solve_fixed_grid_computes_terminal_values_correctly(
@@ -12,3 +19,53 @@ def test_solve_fixed_grid_computes_terminal_values_correctly(
 
     assert jnp.allclose(solution.t[-1], t_ref)
     assert jnp.allclose(solution.u[-1], u_ref, atol=atol, rtol=rtol)
+
+
+def test_solve_fixed_grid_differentiable(ode_problem, fixed_grid):
+    vf, u0, t0, t1, f_args = ode_problem
+    solver = solvers.MLESolver(
+        strategy=filters.Filter(
+            extrapolation=isotropic.IsoIBM.from_params(num_derivatives=1)
+        )
+    )
+
+    fn = jax.jit(
+        functools.partial(
+            _param_to_sol,
+            solver=solver,
+            fixed_grid=fixed_grid,
+            vf=vf,
+            parameters=f_args,
+        )
+    )
+
+    fx = fn(u0[0])
+    dfx_fwd = jax.jit(jax.jacfwd(fn, argnums=0))(u0[0])
+    dfx_rev = jax.jit(jax.jacrev(fn, argnums=0))(u0[0])
+
+    # print("FwdJac-shape", _tree_shape(dfx_fwd))
+    # print("RevJac-shape", _tree_shape(dfx_rev))
+    # print("In-shape:", _tree_shape(u0[0]))
+    # print("Out-shape:", _tree_shape(fx))
+    # print(dfx_fwd)
+
+    out_shape = _tree_shape(fx)
+    in_shape = _tree_shape(u0[0])
+    assert _tree_all_tree_map(jnp.allclose, dfx_fwd, dfx_rev)
+    assert _tree_shape(dfx_fwd) == out_shape + in_shape
+
+
+def _param_to_sol(u0, parameters, vf, solver, fixed_grid):
+    solution = ivpsolve.solve_fixed_grid(
+        vf, (u0,), ts=fixed_grid, parameters=parameters, solver=solver
+    )
+    return solution.u
+
+
+def _tree_shape(tree):
+    return jax.tree_util.tree_map(jnp.shape, tree)
+
+
+def _tree_all_tree_map(fn, *tree):
+    tree_of_bools = jax.tree_util.tree_map(fn, *tree)
+    return jax.tree_util.tree_all(tree_of_bools)
