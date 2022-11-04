1720c0a057e5f17e0a6b236061c0e3e50b4d1712
Nicholas Krämer <peter-nicholas.kraemer@uni-tuebingen.de>
2022-11-04 09:45:38 +0100

    Cache reference solution in terminal value

--- tests/test_simulate_terminal_values.py
+++ tests/test_simulate_terminal_values.py
@@ -1,26 +1,63 @@
 """Tests for IVP solvers."""
 
+import jax
 import jax.numpy as jnp
+import pytest_cases
+from diffeqzoo import ivps
 from jax.experimental.ode import odeint
-from pytest_cases import parametrize_with_cases
 
 from odefilter import ivpsolve
 
 
-@parametrize_with_cases("vf, u0, t0, t1, p", cases=".ivp_cases", prefix="problem_")
-@parametrize_with_cases("solver", cases=".solver_cases")
-def test_simulate_terminal_values(vf, u0, t0, t1, p, solver):
+@pytest_cases.fixture(scope="session", name="ode_problem")
+def fixture_ode_problem():
+    f, u0, (t0, t1), f_args = ivps.lotka_volterra()
+    t1 = 2.0
+
+    @jax.jit
+    def vf(x, *, t, p):
+        return f(x, *p)
+
+    # Only very short time-intervals are sufficient for a unit test.
+    return vf, (u0,), t0, t1, f_args
+
+
+@pytest_cases.fixture(scope="session", name="tolerance")
+def fixture_tolerance():
+    return 1e-5, 1e-3
+
+
+@pytest_cases.fixture(scope="session", name="reference")
+def fixture_reference(ode_problem, tolerance):
+    vf, (u0,), t0, t1, f_args = ode_problem
+    atol, rtol = tolerance
+
+    @jax.jit
     def func(y, t, *p):
         return vf(y, t=t, p=p)
 
-    odeint_solution = odeint(
-        func, u0[0], jnp.asarray([t0, t1]), *p, atol=1e-6, rtol=1e-6
-    )
+    ts = jnp.asarray([t0, t1])
+    odeint_solution = odeint(func, u0, ts, *f_args, atol=atol, rtol=rtol)
     ys_reference = odeint_solution[-1, :]
+    return t1, ys_reference
+
+
+@pytest_cases.fixture(scope="session", name="solution")
+@pytest_cases.parametrize_with_cases("solver", cases=".solver_cases")
+def fixture_solution(ode_problem, tolerance, solver):
+    vf, u0, t0, t1, f_args = ode_problem
+    atol, rtol = tolerance
 
     solution = ivpsolve.simulate_terminal_values(
-        vf, u0, t0=t0, t1=t1, parameters=p, solver=solver
+        vf, u0, t0=t0, t1=t1, parameters=f_args, solver=solver, atol=atol, rtol=rtol
     )
+    return solution.t, solution.u
 
-    assert solution.t == t1
-    assert jnp.allclose(solution.u, ys_reference, atol=1e-3, rtol=1e-3)
+
+def test_simulate_terminal_values(reference, solution, tolerance):
+    t_ref, u_ref = reference
+    t, u = solution
+    atol, rtol = tolerance
+
+    assert t_ref == t
+    assert jnp.allclose(u, u_ref, atol=10 * atol, rtol=10 * rtol)
