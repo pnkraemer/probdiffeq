3c6fb44e9e6ce965d9923ff2eb755640893c24e8
Nicholas Krämer <nicokraemer@gmx.net>
2023-01-15 10:11:52 +0100

    Refactor test case tags (#369)
    * More verbose case tags
    
    * More explicit types
    
    * Removed ivpsolve.ODEFunction

--- tests/solver_cases.py
+++ tests/solver_cases.py
@@ -1,69 +1,76 @@
 """Solver test cases."""
+from typing import Literal, NamedTuple
+
 import pytest_cases
 
 from probdiffeq import cubature, solvers
 from probdiffeq.implementations import recipes
 from probdiffeq.strategies import filters, smoothers
 
-# Note: All ODE test problems will be two-dimensional.
+
+class Tag(NamedTuple):
+    strategy: Literal["filter", "smoother", "fixedpoint"]
+    linearisation_order: Literal["zeroth", "first"]
+    ode_shape: Literal[(2,)]  # todo: scalar problems
+    ode_order: Literal[1]  # todo: second-order problems
 
 
-@pytest_cases.case(tags=["filter"])
+@pytest_cases.case(tags=Tag("filter", "zeroth", ode_shape=(2,), ode_order=1))
 def case_mle_filter_ts0_iso():
     strategy = filters.Filter(recipes.IsoTS0.from_params())
     return solvers.MLESolver(strategy=strategy)
 
 
-@pytest_cases.case(tags=["smoother"])
+@pytest_cases.case(tags=Tag("smoother", "zeroth", ode_shape=(2,), ode_order=1))
 def case_dynamic_smoother_ts0_iso():
     implementation = recipes.IsoTS0.from_params()
     strategy = smoothers.Smoother(implementation=implementation)
     return solvers.DynamicSolver(strategy)
 
 
-@pytest_cases.case(tags=["fixedpoint"])
+@pytest_cases.case(tags=Tag("fixedpoint", "zeroth", ode_shape=(2,), ode_order=1))
 def case_fixedpoint_ts0_iso():
     implementation = recipes.IsoTS0.from_params()
     strategy = smoothers.FixedPointSmoother(implementation=implementation)
     return solvers.Solver(strategy=strategy, output_scale_sqrtm=100.0)
 
 
-@pytest_cases.case(tags=["filter"])
+@pytest_cases.case(tags=Tag("filter", "zeroth", ode_shape=(2,), ode_order=1))
 def case_dynamic_filter_ts0_batch():
     implementation = recipes.BatchTS0.from_params(ode_shape=(2,), num_derivatives=3)
     strategy = filters.Filter(implementation=implementation)
     return solvers.DynamicSolver(strategy=strategy)
 
 
-@pytest_cases.case(tags=["smoother"])
+@pytest_cases.case(tags=Tag("smoother", "zeroth", ode_shape=(2,), ode_order=1))
 def case_mle_smoother_ts0_batch():
     implementation = recipes.BatchTS0.from_params(ode_shape=(2,))
     strategy = smoothers.Smoother(implementation=implementation)
     return solvers.MLESolver(strategy=strategy)
 
 
-@pytest_cases.case(tags=["fixedpoint"])
+@pytest_cases.case(tags=Tag("fixedpoint", "zeroth", ode_shape=(2,), ode_order=1))
 def case_mle_fixedpoint_ts0_batch():
     implementation = recipes.BatchTS0.from_params(ode_shape=(2,))
     strategy = smoothers.FixedPointSmoother(implementation=implementation)
     return solvers.MLESolver(strategy=strategy)
 
 
-@pytest_cases.case(tags=["filter"])
+@pytest_cases.case(tags=Tag("filter", "first", ode_shape=(2,), ode_order=1))
 def case_dynamic_filter_ts1_vect():
     implementation = recipes.VectTS1.from_params(ode_shape=(2,))
     strategy = filters.Filter(implementation=implementation)
     return solvers.DynamicSolver(strategy=strategy)
 
 
-@pytest_cases.case(tags=["filter"])
+@pytest_cases.case(tags=Tag("filter", "first", ode_shape=(2,), ode_order=1))
 def case_mle_filter_slr1_vect():
     implementation = recipes.VectSLR1.from_params(ode_shape=(2,))
     strategy = filters.Filter(implementation=implementation)
     return solvers.MLESolver(strategy=strategy)
 
 
-@pytest_cases.case(tags=["filter"])
+@pytest_cases.case(tags=Tag("filter", "first", ode_shape=(2,), ode_order=1))
 def case_dynamic_filter_slr1_vect_ut():
     cube = cubature.UnscentedTransform.from_params(input_shape=(2,))
     implementation = recipes.VectSLR1.from_params(cubature=cube, ode_shape=(2,))
@@ -71,7 +78,7 @@ def case_dynamic_filter_slr1_vect_ut():
     return solvers.DynamicSolver(strategy=strategy)
 
 
-@pytest_cases.case(tags=["filter"])
+@pytest_cases.case(tags=Tag("filter", "first", ode_shape=(2,), ode_order=1))
 def case_dynamic_filter_slr1_ut_batch():
     cube = cubature.UnscentedTransform.from_params_batch(input_shape=(2,))
     implementation = recipes.BatchSLR1.from_params(cubature=cube, ode_shape=(2,))
@@ -79,14 +86,14 @@ def case_dynamic_filter_slr1_ut_batch():
     return solvers.DynamicSolver(strategy=strategy)
 
 
-@pytest_cases.case(tags=["filter"])
+@pytest_cases.case(tags=Tag("filter", "first", ode_shape=(2,), ode_order=1))
 def case_dynamic_filter_slr1_batch():
     implementation = recipes.BatchSLR1.from_params(ode_shape=(2,))
     strategy = filters.Filter(implementation=implementation)
     return solvers.DynamicSolver(strategy=strategy)
 
 
-@pytest_cases.case(tags=["filter"])
+@pytest_cases.case(tags=Tag("filter", "first", ode_shape=(2,), ode_order=1))
 def case_dynamic_filter_slr1_vect_gh():
     cube = cubature.GaussHermite.from_params(input_shape=(2,))
     implementation = recipes.VectSLR1.from_params(cubature=cube, ode_shape=(2,))
@@ -94,14 +101,14 @@ def case_dynamic_filter_slr1_vect_gh():
     return solvers.DynamicSolver(strategy=strategy)
 
 
-@pytest_cases.case(tags=["filter"])
+@pytest_cases.case(tags=Tag("filter", "zeroth", ode_shape=(2,), ode_order=1))
 def case_dynamic_filter_slr0_vect():
     implementation = recipes.VectSLR0.from_params(ode_shape=(2,))
     strategy = filters.Filter(implementation=implementation)
     return solvers.DynamicSolver(strategy=strategy)
 
 
-@pytest_cases.case(tags=["filter"])
+@pytest_cases.case(tags=Tag("filter", "zeroth", ode_shape=(2,), ode_order=1))
 def case_dynamic_filter_slr0_vect_gh():
     cube = cubature.GaussHermite.from_params(input_shape=(2,))
     implementation = recipes.VectSLR0.from_params(cubature=cube, ode_shape=(2,))
@@ -109,21 +116,21 @@ def case_dynamic_filter_slr0_vect_gh():
     return solvers.DynamicSolver(strategy=strategy)
 
 
-@pytest_cases.case(tags=["smoother"])
+@pytest_cases.case(tags=Tag("smoother", "first", ode_shape=(2,), ode_order=1))
 def case_mle_smoother_ts1_vect():
     implementation = recipes.VectTS1.from_params(ode_shape=(2,))
     strategy = smoothers.Smoother(implementation=implementation)
     return solvers.MLESolver(strategy=strategy)
 
 
-@pytest_cases.case(tags=["fixedpoint"])
+@pytest_cases.case(tags=Tag("fixedpoint", "first", ode_shape=(2,), ode_order=1))
 def case_mle_fixedpoint_ts1_vect():
     implementation = recipes.VectTS1.from_params(ode_shape=(2,))
     strategy = smoothers.FixedPointSmoother(implementation=implementation)
     return solvers.MLESolver(strategy=strategy)
 
 
-@pytest_cases.case(tags=["fixedpoint"])
+@pytest_cases.case(tags=Tag("fixedpoint", "zeroth", ode_shape=(2,), ode_order=1))
 def case_mle_fixedpoint_ts0_vect():
     implementation = recipes.VectTS0.from_params(ode_shape=(2,))
     strategy = smoothers.FixedPointSmoother(implementation=implementation)
