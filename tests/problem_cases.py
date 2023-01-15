460ffa32c1835778af49461c5852f4b2b25ce3c0
Nicholas Krämer <nicokraemer@gmx.net>
2023-01-15 11:10:51 +0100

    Solution in problem cases via diffrax

--- tests/problem_cases.py
+++ tests/problem_cases.py
@@ -1,9 +1,10 @@
 """Test cases for ODE problems."""
 
 
-from typing import Literal, NamedTuple
+from typing import Callable, Literal, NamedTuple, Tuple
 
 import diffeqzoo.ivps
+import diffrax
 import jax
 import pytest_cases
 import pytest_cases.filters
@@ -15,6 +16,15 @@ class Tag(NamedTuple):
     stiff: Literal[True, False]
 
 
+class ODEProblem(NamedTuple):
+    vector_field: Callable
+    initial_values: Tuple
+    t0: float
+    t1: float
+    args: Tuple
+    solution: Callable
+
+
 @pytest_cases.case(tags=(Tag(shape=(2,), order=1, stiff=False),))
 def case_lotka_volterra():
     f, u0, (t0, t1), f_args = diffeqzoo.ivps.lotka_volterra()
@@ -24,5 +34,26 @@ def case_lotka_volterra():
     def vf(x, *, t, p):  # pylint: disable=unused-argument
         return f(x, *p)
 
+    def vf_diffrax(t, y, args):
+        return vf(y, t=t, p=args)
+
+    term = diffrax.ODETerm(vf_diffrax)
+    solver = diffrax.Dopri5()
+    solution_object = diffrax.diffeqsolve(
+        term,
+        solver,
+        t0=t0,
+        t1=t1,
+        dt0=0.1,
+        y0=u0,
+        args=f_args,
+        saveat=diffrax.SaveAt(dense=True),
+        stepsize_controller=diffrax.PIDController(atol=1e-10, rtol=1e-10),
+    )
+
+    @jax.jit
+    def solution(t):
+        return solution_object.evaluate(t)
+
     # Only very short time-intervals are sufficient for a unit test.
-    return vf, (u0,), t0, t1, f_args
+    return ODEProblem(vf, (u0,), t0, t1, f_args, solution)
