29467c38120428bef3ce764432f70a1d8c30ee2c
Nicholas Krämer <nicokraemer@gmx.net>
2023-03-13 17:02:03 +0100

    Control variates

--- probdiffeq/implementations/_hutch.py
+++ probdiffeq/implementations/_hutch.py
@@ -1,33 +1,51 @@
-"""Hutchinson-style trace and diagonal estimation."""
+"""Hutchinson-style trace and diagonal estimation.
+
+See e.g.
+
+http://www.nowozin.net/sebastian/blog/thoughts-on-trace-estimation-in-deep-learning.html
+
+"""
+
+import functools
 
 import jax
 import jax.numpy as jnp
 
 
-def trace(*args, matvec_fn, **kwargs):
+@functools.partial(
+    jax.jit,
+    static_argnames=(
+        "matvec_fn",
+        "tangents_shape",
+        "tangents_dtype",
+        "batch_size",
+        "generate_samples_fn",
+    ),
+)
+def trace(matvec_fn, **kwargs):
     """Estimate the trace of a matrix stochastically."""
 
     def Q(x):
         return jnp.dot(x, matvec_fn(x))
 
-    return _hutchinson(Q, *args, **kwargs)
+    return _stochastic_estimate(Q, **kwargs)
 
 
-def diagonal(*args, matvec_fn, **kwargs):
+def diagonal(matvec_fn, **kwargs):
     """Estimate the diagonal of a matrix stochastically."""
 
     def Q(x):
         return x * matvec_fn(x)
 
-    return _hutchinson(Q, *args, **kwargs)
+    return _stochastic_estimate(Q, **kwargs)
 
 
-def _hutchinson(*args, batch_keys, **kwargs):
-    """Hutchinson-style trace estimation."""
+def _stochastic_estimate(*args, batch_keys, **kwargs):
+    """Hutchinson-style stochastic estimation."""
 
     @jax.jit
     def f(key):
-        return _hutchinson_batch(*args, key=key, **kwargs)
+        return _stochastic_estimate_batch(*args, key=key, **kwargs)
 
     # Compute batches sequentially to reduce memory.
     batch_keys = jnp.atleast_2d(batch_keys)
@@ -37,7 +55,7 @@ def _hutchinson(*args, batch_keys, **kwargs):
     return jnp.mean(means, axis=0)
 
 
-def _hutchinson_batch(
+def _stochastic_estimate_batch(
     Q,
     /,
     *,
@@ -50,3 +68,53 @@ def _hutchinson_batch(
     shape = (batch_size,) + tangents_shape
     samples = generate_samples_fn(key, shape=shape, dtype=tangents_dtype)
     return jnp.mean(jax.vmap(Q)(samples), axis=0)
+
+
+@functools.partial(
+    jax.jit,
+    static_argnames=(
+        "matvec_fn",
+        "tangents_shape",
+        "tangents_dtype",
+        "generate_samples_fn",
+    ),
+)
+def trace_and_diagonal(
+    matvec_fn,
+    *,
+    tangents_shape,
+    tangents_dtype,
+    keys,
+    generate_samples_fn,
+):
+    """Jointly estimate the trace and the diagonal stochastically.
+
+    The advantage of computing both quantities simultaneously is
+    that the diagonal estimate
+    may serve as a control variate for the trace estimate,
+    thus reducing the variance of the estimator
+    (and thereby accelerating convergence.)
+
+    See:
+    Adams et al., Estimating the Spectral Density of Large Implicit Matrices, 2018.
+    """
+    zeros = jnp.zeros(shape=tangents_shape, dtype=tangents_dtype)
+    init = (0.0, zeros, 0)  # trace, diag, n
+
+    def body_fn(carry, key):
+        trace, diag, n = carry
+
+        z = generate_samples_fn(key, shape=tangents_shape, dtype=tangents_dtype)
+        y = z * (matvec_fn(z) - diag * z)
+
+        trace_new = _increment(trace, n, jnp.sum(y) + sum(diag))
+        diag_new = _increment(diag, n, y + diag)
+
+        return (trace_new, diag_new, n + 1), ()
+
+    (trace_final, diag_final, _), _ = jax.lax.scan(body_fn, init=init, xs=keys)
+    return trace_final, diag_final
+
+
+def _increment(old, count, incoming):
+    return (old * count + incoming) / (count + 1)
--- tests/test_implementations/test_hutch.py
+++ tests/test_implementations/test_hutch.py
@@ -21,29 +21,56 @@ def fixture_rng_key():
     return jax.random.PRNGKey(seed=1)
 
 
-@pytest_cases.parametrize("num_batches", [1, 1_000])
-@pytest_cases.parametrize("batch_size", [1, 1_000])
-@pytest_cases.parametrize("dim", [1, 100])
+@pytest_cases.parametrize("num_samples", [10_000])
+@pytest_cases.parametrize("dim", [5])
 @pytest_cases.parametrize(
     "generate_samples_fn", [jax.random.normal, jax.random.rademacher]
 )
-def test_trace(fn, rng_key, num_batches, batch_size, dim, generate_samples_fn):
+def test_trace_and_diagonal(fn, rng_key, num_samples, dim, generate_samples_fn):
     # Linearise function
     x0 = jax.random.uniform(rng_key, shape=(dim,))
     _, jvp = jax.linearize(fn, x0)
     J = jax.jacfwd(fn)(x0)
 
     # Sequential batches
-    keys = jax.random.split(rng_key, num=num_batches)
+    keys = jax.random.split(rng_key, num=num_samples)
 
     # Estimate the trace
-    estimate = hutch.trace(
+    trace, diag = hutch.trace_and_diagonal(
         matvec_fn=jvp,
         tangents_shape=jnp.shape(x0),
         tangents_dtype=jnp.dtype(x0),
-        batch_keys=keys,
-        batch_size=batch_size,
+        keys=keys,
         generate_samples_fn=generate_samples_fn,
     )
-    truth = jnp.trace(J)
-    assert jnp.allclose(estimate, truth, atol=1e-1, rtol=1e-1)
+    assert jnp.allclose(diag, jnp.diag(J)), jnp.linalg.norm(diag - jnp.diag(J))
+    assert jnp.allclose(trace, jnp.trace(J)), jnp.linalg.norm(trace - jnp.trace(J))
+
+
+#
+# @pytest_cases.parametrize("num_batches", [1, 1_000])
+# @pytest_cases.parametrize("batch_size", [1, 1_000])
+# @pytest_cases.parametrize("dim", [1, 100])
+# @pytest_cases.parametrize(
+#     "generate_samples_fn", [jax.random.normal, jax.random.rademacher]
+# )
+# def test_trace(fn, rng_key, num_batches, batch_size, dim, generate_samples_fn):
+#     # Linearise function
+#     x0 = jax.random.uniform(rng_key, shape=(dim,))
+#     _, jvp = jax.linearize(fn, x0)
+#     J = jax.jacfwd(fn)(x0)
+#
+#     # Sequential batches
+#     keys = jax.random.split(rng_key, num=num_batches)
+#
+#     # Estimate the trace
+#     estimate = hutch.trace(
+#         matvec_fn=jvp,
+#         tangents_shape=jnp.shape(x0),
+#         tangents_dtype=jnp.dtype(x0),
+#         batch_keys=keys,
+#         batch_size=batch_size,
+#         generate_samples_fn=generate_samples_fn,
+#     )
+#     truth = jnp.trace(J)
+#     assert jnp.allclose(estimate, truth, atol=1e-1, rtol=1e-1)
