b1cc5f7703ef505bfa5c9941028743c4ded4bfb8
Nicholas Krämer <nicokraemer@gmx.net>
2023-03-16 10:30:19 +0100

    Tests for dense output updated

--- tests/test_dense_output.py
+++ tests/test_dense_output.py
@@ -1,15 +1,74 @@
 """Tests for IVP solvers."""
 import jax
 import jax.numpy as jnp
+import pytest
 import pytest_cases
 import pytest_cases.filters
 
-from probdiffeq import dense_output
+from probdiffeq import dense_output, solution_routines, test_util
 from probdiffeq.strategies import filters, smoothers
 
 
-def test_offgrid_marginals_filter(solution_solve):
-    solution, solver = solution_solve
+@pytest_cases.fixture(scope="session", name="solution_native_python_while_loop")
+@pytest_cases.parametrize_with_cases("ode_problem", cases=".problem_cases")
+def fixture_solution_native_python_while_loop(ode_problem):
+    solver = test_util.generate_solver(num_derivatives=1)
+    solution = solution_routines.solve_with_python_while_loop(
+        ode_problem.vector_field,
+        ode_problem.initial_values,
+        t0=ode_problem.t0,
+        t1=ode_problem.t1,
+        parameters=ode_problem.args,
+        solver=solver,
+        atol=1e-1,
+        rtol=1e-2,
+    )
+    return solution, solver
+
+
+def test_solution_is_iterable(solution_native_python_while_loop):
+    solution, _ = solution_native_python_while_loop
+    assert isinstance(solution[0], type(solution))
+    assert len(solution) == len(solution.t)
+
+
+def test_getitem_raises_error_for_nonbatched_solutions(
+    solution_native_python_while_loop,
+):
+    """__getitem__ only works for batched solutions."""
+    solution, _ = solution_native_python_while_loop
+    with pytest.raises(ValueError):
+        _ = solution[0][0]
+    with pytest.raises(ValueError):
+        _ = solution[0, 0]
+
+
+def test_loop_over_solution_is_possible(solution_native_python_while_loop):
+    solution, _ = solution_native_python_while_loop
+
+    i = 0
+    for i, sol in zip(range(2 * len(solution)), solution):
+        assert isinstance(sol, type(solution))
+
+    assert i == len(solution) - 1
+
+
+# Maybe this test should be in a different test suite, but it does not really matter...
+def test_marginal_nth_derivative_of_solution(solution_native_python_while_loop):
+    solution, _ = solution_native_python_while_loop
+
+    # Assert that the marginals have the same shape as the qoi.
+    for i in (0, 1):
+        derivatives = solution.marginals.marginal_nth_derivative(i)
+        assert derivatives.mean.shape == solution.u.shape
+
+    # if the requested derivative is not in the state-space model, raise a ValueError
+    with pytest.raises(ValueError):
+        solution.marginals.marginal_nth_derivative(100)
+
+
+def test_offgrid_marginals_filter(solution_native_python_while_loop):
+    solution, solver = solution_native_python_while_loop
     t0, t1 = solution.t[0], solution.t[-1]
 
     # todo: this is hacky. But the tests get faster?
@@ -41,8 +100,8 @@ def test_offgrid_marginals_filter(solution_solve):
         assert not jnp.allclose(u[0], solution.u[1], atol=1e-3, rtol=1e-3)
 
 
-def test_offgrid_marginals_smoother(solution_solve):
-    solution, solver = solution_solve
+def test_offgrid_marginals_smoother(solution_native_python_while_loop):
+    solution, solver = solution_native_python_while_loop
     t0, t1 = solution.t[0], solution.t[-1]
 
     # todo: this is hacky. But the tests get faster?
@@ -74,23 +133,37 @@ def test_offgrid_marginals_smoother(solution_solve):
         assert jnp.allclose(u[-1], solution.u[-1], atol=1e-3, rtol=1e-3)
 
 
+@pytest_cases.fixture(scope="session", name="solution_save_at")
+@pytest_cases.parametrize_with_cases("ode_problem", cases=".problem_cases")
+def fixture_solution_save_at(ode_problem):
+    solver = test_util.generate_solver(strategy_factory=smoothers.FixedPointSmoother)
+
+    save_at = jnp.linspace(ode_problem.t0, ode_problem.t1, endpoint=True, num=4)
+    solution = solution_routines.solve_and_save_at(
+        ode_problem.vector_field,
+        ode_problem.initial_values,
+        save_at=save_at,
+        parameters=ode_problem.args,
+        solver=solver,
+        atol=1e-1,
+        rtol=1e-2,
+    )
+    return solution, solver
+
+
 @pytest_cases.parametrize("shape", [(), (2,), (2, 2)], ids=["()", "(n,)", "(n,n)"])
 def test_grid_samples(solution_save_at, shape):
     solution, solver = solution_save_at
 
-    # todo: this is hacky. But the tests get faster?
-    if isinstance(solver.strategy, smoothers.FixedPointSmoother):
-        key = jax.random.PRNGKey(seed=15)
-        u, samples = dense_output.sample(
-            key, solution=solution, solver=solver, shape=shape
-        )
-        assert u.shape == shape + solution.u.shape
-        assert samples.shape == shape + solution.marginals.hidden_state.sample_shape
+    key = jax.random.PRNGKey(seed=15)
+    u, samples = dense_output.sample(key, solution=solution, solver=solver, shape=shape)
+    assert u.shape == shape + solution.u.shape
+    assert samples.shape == shape + solution.marginals.hidden_state.sample_shape
 
-        # Todo: test values of the samples by checking a chi2 statistic
-        #  in terms of the joint posterior. But this requires a joint_posterior()
-        #  method, which is only future work I guess. So far we use the eye-test
-        #  in the notebooks, which looks good.
+    # Todo: test values of the samples by checking a chi2 statistic
+    #  in terms of the joint posterior. But this requires a joint_posterior()
+    #  method, which is only future work I guess. So far we use the eye-test
+    #  in the notebooks, which looks good.
 
 
 def test_negative_marginal_log_likelihood(solution_save_at):
--- tests/test_edges.py
+++ tests/test_edges.py
@@ -5,7 +5,7 @@ Place all tests that have no better place here.
 import pytest
 import pytest_cases
 
-from probdiffeq import solution_routines, test_util
+from probdiffeq import test_util
 
 
 @pytest_cases.parametrize("incr", [1, -1])
@@ -17,56 +17,3 @@ def test_incorrect_number_of_taylor_coefficients_init(incr, n):
     init_fn = solver.strategy.implementation.extrapolation.init_hidden_state
     with pytest.raises(ValueError):
         init_fn(taylor_coefficients=tcoeffs_wrong_length)
-
-
-# Assert correct behaviour of the solution object (raise error for wrong slicing etc.)
-
-
-@pytest_cases.fixture(name="dummy_solution")
-@pytest_cases.parametrize_with_cases("ode_problem", cases=".problem_cases")
-def fixture_dummy_solution(ode_problem):
-    solver = test_util.generate_solver(num_derivatives=1)
-    solution = solution_routines.solve_with_python_while_loop(
-        ode_problem.vector_field,
-        ode_problem.initial_values,
-        t0=ode_problem.t0,
-        t1=ode_problem.t1,
-        parameters=ode_problem.args,
-        solver=solver,
-        atol=1e-1,
-        rtol=1e-2,
-    )
-    return solution
-
-
-def test_solution_is_iterable(dummy_solution):
-    assert isinstance(dummy_solution[0], type(dummy_solution))
-    assert len(dummy_solution) == len(dummy_solution.t)
-
-
-def test_getitem_raises_error_for_nonbatched_solutions(dummy_solution):
-    """__getitem__ only works for batched solutions."""
-    with pytest.raises(ValueError):
-        _ = dummy_solution[0][0]
-    with pytest.raises(ValueError):
-        _ = dummy_solution[0, 0]
-
-
-def test_loop_over_solution_is_possible(dummy_solution):
-    i = 0
-    for i, sol in zip(range(2 * len(dummy_solution)), dummy_solution):
-        assert isinstance(sol, type(dummy_solution))
-
-    assert i == len(dummy_solution) - 1
-
-
-# Maybe this test should be in a different test suite, but it does not really matter...
-def test_marginal_nth_derivative_of_solution(dummy_solution):
-    # Assert that the marginals have the same shape as the qoi.
-    for i in (0, 1):
-        derivatives = dummy_solution.marginals.marginal_nth_derivative(i)
-        assert derivatives.mean.shape == dummy_solution.u.shape
-
-    # if the requested derivative is not in the state-space model, raise a ValueError
-    with pytest.raises(ValueError):
-        dummy_solution.marginals.marginal_nth_derivative(100)
