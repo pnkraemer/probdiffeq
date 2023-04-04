92289e8e7af39326778b13d66067c6046d6baab2
Nicholas Krämer <nicokraemer@gmx.net>
2023-04-04 15:14:39 +0200

    Tests for logpdfs

--- tests/test_implementations/test_logpdfs.py
+++ tests/test_implementations/test_logpdfs.py
@@ -6,35 +6,85 @@ Necessary because the implementation has been faulty in the past. Never again.
 import jax.numpy as jnp
 import jax.scipy.stats
 
+from probdiffeq.backend import testing
 from probdiffeq.implementations import _scalar
 from probdiffeq.implementations.dense import _vars as vars_dense
 from probdiffeq.implementations.iso import _vars as vars_iso
 
 
-def test_logpdf_dense():
-    u = jnp.arange(1.0, 4.0)
-    m = u + 1.0
-    cov_cholesky = jnp.linalg.cholesky(u[:, None] * u[None, :] + jnp.eye(3))
+@testing.fixture(name="setup")
+@testing.parametrize("d", [1, 10])
+def fixture_setup(d):
+    m = jnp.arange(1.0, 1.0 + d)
 
-    pdf1 = vars_dense.DenseNormal(m, cov_cholesky).logpdf(u)
-    pdf2 = jax.scipy.stats.multivariate_normal.logpdf(
-        u, mean=m, cov=cov_cholesky @ cov_cholesky.T
-    )
+    X = m[:, None] * m[None, :] + jnp.eye(d)
+    # X *= jnp.arange(d)[:, None]  # singular
+
+    cov_cholesky = 1e-10 * jnp.linalg.qr(X, mode="r").T
+    return m, cov_cholesky
+
+
+def test_logpdf_dense(setup):
+    m, cov_cholesky = setup
+
+    def fn1(x):
+        return vars_dense.DenseNormal(m, cov_cholesky).logpdf(x)
+
+    def fn2(x):
+        return jax.scipy.stats.multivariate_normal.logpdf(
+            x, mean=m, cov=cov_cholesky @ cov_cholesky.T
+        )
+
+    u = m + 1e-3
+    pdf1 = fn1(u)
+    pdf2 = fn2(u)
+    assert jnp.allclose(pdf1, pdf2)
+
+    pdf1 = jax.grad(fn1)(u)
+    pdf2 = jax.grad(fn2)(u)
     assert jnp.allclose(pdf1, pdf2)
 
 
-def test_logpdf_iso():
-    u = jnp.arange(1.0, 4.0)
-    m = u + 1.0
-    cov_cholesky = 4.0
-    pdf1 = vars_iso.IsoNormalQOI(m, cov_cholesky).logpdf(u)
-    pdf2 = jax.scipy.stats.multivariate_normal.logpdf(
-        u, mean=m, cov=cov_cholesky**2 * jnp.eye(3)
-    )
+def test_logpdf_iso(setup):
+    m, cov_cholesky = setup
+    variance = jnp.trace(cov_cholesky)
+
+    def fn1(x):
+        return vars_iso.IsoNormalQOI(m, variance).logpdf(x)
+
+    def fn2(x):
+        return jax.scipy.stats.multivariate_normal.logpdf(
+            x, mean=m, cov=variance**2 * jnp.eye(len(m))
+        )
+
+    u = m + 1e-3
+    pdf1 = fn1(u)
+    pdf2 = fn2(u)
+    assert jnp.allclose(pdf1, pdf2)
+
+    pdf1 = jax.grad(fn1)(u)
+    pdf2 = jax.grad(fn2)(u)
     assert jnp.allclose(pdf1, pdf2)
 
 
-def test_logpdf_scalar():
-    pdf1 = _scalar.NormalQOI(2.0, 3.0).logpdf(5.0)
-    pdf2 = jax.scipy.stats.multivariate_normal.logpdf(5.0, mean=2.0, cov=3.0**2)
+def test_logpdf_scalar(setup):
+    m, cov_cholesky = setup
+    mean = jnp.linalg.norm(m)
+    variance = jnp.trace(cov_cholesky)
+
+    def fn1(x):
+        return _scalar.NormalQOI(mean, variance).logpdf(x)
+
+    def fn2(x):
+        return jax.scipy.stats.multivariate_normal.logpdf(
+            x, mean=mean, cov=variance**2
+        )
+
+    u = mean + 1e-3
+    pdf1 = fn1(u)
+    pdf2 = fn2(u)
+    assert jnp.allclose(pdf1, pdf2)
+
+    pdf1 = jax.grad(fn1)(mean)
+    pdf2 = jax.grad(fn2)(mean)
     assert jnp.allclose(pdf1, pdf2)
