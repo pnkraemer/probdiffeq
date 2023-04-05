a54b4c7a90bbbfe2a8ff6b2ff679d7af51264017
Nicholas Krämer <nicokraemer@gmx.net>
2023-04-05 11:25:40 +0200

    Solve fixed grid updated

--- tests/test_ivpsolve/test_solve_fixed_grid.py
+++ tests/test_ivpsolve/test_solve_fixed_grid.py
@@ -1,44 +1,112 @@
 """Tests for solving IVPs on fixed grids."""
 import functools
+from typing import Any, NamedTuple
 
 import jax
 import jax.numpy as jnp
 import jax.test_util
 
-from probdiffeq import ivpsolve, test_util
+from probdiffeq import ivpsolve, ivpsolvers, test_util
 from probdiffeq.backend import testing
 from probdiffeq.implementations import recipes
 from probdiffeq.strategies import filters, smoothers
 
 
-@testing.fixture(scope="session", name="solution_fixed_grid")
-@testing.parametrize_with_cases("ode_problem", cases="..problem_cases")
-@testing.parametrize_with_cases("impl_fn", cases="..impl_cases")
+class _SolveFixedGridConfig(NamedTuple):
+    ode_problem: Any
+    solver_fn: Any
+    impl_fn: Any
+    strat_fn: Any
+    solver_config: Any
+
+
+@testing.case
+@testing.parametrize_with_cases("ode_problem", cases="..problem_cases", has_tag="nd")
+@testing.parametrize_with_cases("impl_fn", cases="..impl_cases", has_tag="nd")
+def case_setup_all_implementations_nd(ode_problem, impl_fn, solver_config):
+    return _SolveFixedGridConfig(
+        ode_problem=ode_problem,
+        solver_fn=ivpsolvers.MLESolver,
+        impl_fn=impl_fn,
+        strat_fn=filters.Filter,
+        solver_config=solver_config,
+    )
+
+
+@testing.case
+@testing.parametrize_with_cases(
+    "ode_problem", cases="..problem_cases", has_tag="scalar"
+)
+@testing.parametrize_with_cases("impl_fn", cases="..impl_cases", has_tag="scalar")
+def case_setup_all_implementations_scalar(ode_problem, impl_fn, solver_config):
+    return _SolveFixedGridConfig(
+        ode_problem=ode_problem,
+        solver_fn=ivpsolvers.MLESolver,
+        impl_fn=impl_fn,
+        strat_fn=filters.Filter,
+        solver_config=solver_config,
+    )
+
+
+@testing.case
+@testing.parametrize_with_cases("ode_problem", cases="..problem_cases", has_tag="nd")
+@testing.parametrize(
+    "strat_fn", [filters.Filter, smoothers.Smoother, smoothers.FixedPointSmoother]
+)
+def case_setup_all_strategies(ode_problem, strat_fn, solver_config):
+    return _SolveFixedGridConfig(
+        ode_problem=ode_problem,
+        solver_fn=ivpsolvers.MLESolver,
+        impl_fn=recipes.BlockDiagTS0.from_params,
+        strat_fn=strat_fn,
+        solver_config=solver_config,
+    )
+
+
+@testing.case
+@testing.parametrize_with_cases("ode_problem", cases="..problem_cases", has_tag="nd")
 @testing.parametrize_with_cases("solver_fn", cases="..ivpsolver_cases")
-@testing.parametrize("strat_fn", [filters.Filter, smoothers.Smoother])
-def fixture_solution_fixed_grid(
-    ode_problem, solver_fn, impl_fn, strat_fn, solver_config
-):
-    ode_shape = ode_problem.initial_values[0].shape
+def case_setup_all_ivpsolvers(ode_problem, solver_fn, solver_config):
+    return _SolveFixedGridConfig(
+        ode_problem=ode_problem,
+        solver_fn=solver_fn,
+        impl_fn=recipes.BlockDiagTS0.from_params,
+        strat_fn=filters.Filter,
+        solver_config=solver_config,
+    )
+
+
+# Compute the IVP solution for given setups
+
+
+@testing.fixture(scope="session", name="solution_fixed_grid")
+@testing.parametrize_with_cases(
+    "setup", cases=".", prefix="case_setup_", scope="session"
+)
+def fixture_solution_fixed_grid(setup):
+    ode_shape = setup.ode_problem.initial_values[0].shape
     solver = test_util.generate_solver(
-        solver_factory=solver_fn,
-        strategy_factory=strat_fn,
-        impl_factory=impl_fn,
+        solver_factory=setup.solver_fn,
+        strategy_factory=setup.strat_fn,
+        impl_factory=setup.impl_fn,
         ode_shape=ode_shape,
         num_derivatives=4,
     )
 
-    t0, t1 = ode_problem.t0, ode_problem.t1
-    grid = solver_config.grid_for_fixed_grid_fn(t0, t1)
+    t0, t1 = setup.ode_problem.t0, setup.ode_problem.t1
+    grid = setup.solver_config.grid_for_fixed_grid_fn(t0, t1)
 
     solution = ivpsolve.solve_fixed_grid(
-        ode_problem.vector_field,
-        ode_problem.initial_values,
+        setup.ode_problem.vector_field,
+        setup.ode_problem.initial_values,
         grid=grid,
-        parameters=ode_problem.args,
+        parameters=setup.ode_problem.args,
         solver=solver,
     )
-    return (solution.t, solution.u), (grid, jax.vmap(ode_problem.solution)(grid))
+
+    sol = (solution.t, solution.u)
+    sol_ref = (grid, jax.vmap(setup.ode_problem.solution)(grid))
+    return sol, sol_ref
 
 
 def test_terminal_values_correct(solution_fixed_grid, solver_config):
@@ -50,38 +118,35 @@ def test_terminal_values_correct(solution_fixed_grid, solver_config):
 
 # todo: all solver implementations
 @testing.fixture(scope="session", name="parameter_to_solution")
-@testing.parametrize_with_cases("ode_problem", cases="..problem_cases")
-@testing.parametrize("impl_fn", [recipes.BlockDiagTS0.from_params])
-@testing.parametrize_with_cases("solver_fn", cases="..ivpsolver_cases")
-@testing.parametrize("strat_fn", [filters.Filter, smoothers.Smoother])
-def fixture_parameter_to_solution(
-    ode_problem, solver_config, impl_fn, solver_fn, strat_fn
-):
+@testing.parametrize_with_cases(
+    "setup", cases=".", prefix="case_setup_", scope="session"
+)
+def fixture_parameter_to_solution(setup):
     """Parameter-to-solution map. To be differentiated."""
 
     def fn(u0):
-        ode_shape = ode_problem.initial_values[0].shape
+        ode_shape = setup.ode_problem.initial_values[0].shape
         solver = test_util.generate_solver(
-            solver_factory=solver_fn,
-            strategy_factory=strat_fn,
-            impl_factory=impl_fn,
+            solver_factory=setup.solver_fn,
+            strategy_factory=setup.strat_fn,
+            impl_factory=setup.impl_fn,
             ode_shape=ode_shape,
             num_derivatives=1,  # Low order traces more quickly
         )
 
-        t0, t1 = ode_problem.t0, ode_problem.t1
-        grid = solver_config.grid_for_fixed_grid_fn(t0, t1)
+        t0, t1 = setup.ode_problem.t0, setup.ode_problem.t1
+        grid = setup.solver_config.grid_for_fixed_grid_fn(t0, t1)
 
         solution = ivpsolve.solve_fixed_grid(
-            ode_problem.vector_field,
+            setup.ode_problem.vector_field,
             u0,
             grid=grid,
-            parameters=ode_problem.args,
+            parameters=setup.ode_problem.args,
             solver=solver,
         )
         return solution.u
 
-    return fn, ode_problem.initial_values
+    return fn, setup.ode_problem.initial_values
 
 
 def test_jvp(parameter_to_solution):
