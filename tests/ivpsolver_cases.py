53b11f73ff83060296b485a1970aa240024dfb20
Nicholas Krämer <nicokraemer@gmx.net>
2023-04-09 07:54:53 +0200

    Updated test utilities

--- tests/ivpsolver_cases.py
+++ tests/ivpsolver_cases.py
@@ -6,18 +6,20 @@ from probdiffeq.backend import testing
 
 @testing.case(id="MLESolver")
 def case_mle():
-    def factory(strategy, output_scale_sqrtm):
-        return ivpsolvers.MLESolver(strategy)
-
-    return factory
+    return ivpsolvers.MLESolver
+    # def factory(strategy, output_scale_sqrtm):
+    #     return ivpsolvers.MLESolver(strategy)
+    #
+    # return factory
 
 
 @testing.case(id="DynamicSolver")
 def case_dynamic():
-    def factory(strategy, output_scale_sqrtm):
-        return ivpsolvers.DynamicSolver(strategy)
-
-    return factory
+    return ivpsolvers.DynamicSolver
+    # def factory(strategy, output_scale_sqrtm):
+    #     return ivpsolvers.DynamicSolver(strategy)
+    #
+    # return factory
 
 
 @testing.case(id="CalibrationFreeSolver")
