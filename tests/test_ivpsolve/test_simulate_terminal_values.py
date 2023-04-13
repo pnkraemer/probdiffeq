f54b88b9e3b9664c1101d7d95af9dbe30ba07899
Nicholas Krämer <nicokraemer@gmx.net>
2023-04-13 12:52:05 +0200

    Increased test coverage in simulate_terminal_values

--- tests/test_ivpsolve/test_simulate_terminal_values.py
+++ tests/test_ivpsolve/test_simulate_terminal_values.py
@@ -30,12 +30,17 @@ class _SimulateTerminalValuesConfig(NamedTuple):
 @testing.case
 @testing.parametrize_with_cases("ode_problem", cases="..problem_cases", has_tag="nd")
 @testing.parametrize_with_cases("impl_fn", cases="..statespace_cases", has_tag="nd")
-def case_setup_all_statespace_nd(ode_problem, impl_fn, solver_config):
+@testing.parametrize(
+    "strat_fn", [filters.Filter, smoothers.Smoother, smoothers.FixedPointSmoother]
+)
+def case_setup_all_strategy_statespace_nd(
+    ode_problem, impl_fn, strat_fn, solver_config
+):
     return _SimulateTerminalValuesConfig(
         ode_problem=ode_problem,
         solver_fn=ivpsolvers.MLESolver,
         impl_fn=impl_fn,
-        strat_fn=filters.Filter,
+        strat_fn=strat_fn,
         solver_config=solver_config,
         loop_fn=jax.lax.while_loop,
         control=controls.ProportionalIntegral(),
@@ -63,31 +68,16 @@ def case_setup_all_statespace_scalar(ode_problem, impl_fn, solver_config):
 
 @testing.case
 @testing.parametrize_with_cases("ode_problem", cases="..problem_cases", has_tag="nd")
+@testing.parametrize_with_cases("solver_fn", cases="..ivpsolver_cases")
 @testing.parametrize(
     "strat_fn", [filters.Filter, smoothers.Smoother, smoothers.FixedPointSmoother]
 )
-def case_setup_all_strategies(ode_problem, strat_fn, solver_config):
-    return _SimulateTerminalValuesConfig(
-        ode_problem=ode_problem,
-        solver_fn=ivpsolvers.MLESolver,
-        impl_fn=recipes.ts0_blockdiag,
-        strat_fn=strat_fn,
-        solver_config=solver_config,
-        loop_fn=jax.lax.while_loop,
-        control=controls.ProportionalIntegral(),
-        output_scale=1.0,
-    )
-
-
-@testing.case
-@testing.parametrize_with_cases("ode_problem", cases="..problem_cases", has_tag="nd")
-@testing.parametrize_with_cases("solver_fn", cases="..ivpsolver_cases")
-def case_setup_all_ivpsolvers(ode_problem, solver_fn, solver_config):
+def case_setup_all_ivpsolvers_strategy(ode_problem, solver_fn, strat_fn, solver_config):
     return _SimulateTerminalValuesConfig(
         ode_problem=ode_problem,
         solver_fn=solver_fn,
         impl_fn=recipes.ts0_blockdiag,
-        strat_fn=filters.Filter,
+        strat_fn=strat_fn,
         solver_config=solver_config,
         loop_fn=jax.lax.while_loop,
         control=controls.ProportionalIntegral(),
