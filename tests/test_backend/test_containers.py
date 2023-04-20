f507c69f264156bf58540e80eb3b033e1f63429b
Nicholas Krämer <nicokraemer@gmx.net>
2023-04-20 17:07:57 +0200

    Test for nested dataclasses

--- tests/test_backend/test_containers.py
+++ tests/test_backend/test_containers.py
@@ -1,6 +1,7 @@
 """Tests for specific containers."""
 
 import dataclasses
+from typing import Any
 
 import jax.numpy as jnp
 import jax.tree_util
@@ -8,17 +9,19 @@ import jax.tree_util
 from probdiffeq.backend import containers
 
 
+@dataclasses.dataclass
+class MyClass:
+    a: Any
+    b: Any
+
+
+@containers.dataclass_pytree_node
+class MyPyTree:
+    a: Any
+    b: Any
+
+
 def test_dataclass_tree():
-    @dataclasses.dataclass
-    class MyClass:
-        a: float
-        b: float
-
-    @containers.dataclass_pytree_node
-    class MyPyTree:
-        a: float
-        b: float
-
     # Traditional dataclasses are leaves
     dataclass_instance = MyClass([2.0], (3.0, 4.0, 5.0, 6.0))
     children, aux = jax.tree_util.tree_flatten(dataclass_instance)
@@ -32,3 +35,12 @@ def test_dataclass_tree():
     # tree_unflatten works as well.
     back = jax.tree_util.tree_unflatten(aux, children)
     assert jax.tree_util.tree_all(jax.tree_map(jnp.allclose, back, dataclass_pytree))
+
+
+def test_dataclass_nested():
+    """Tree-flattening and unflattening for nested data classes must be as expected."""
+    tree2 = MyPyTree(1, MyPyTree(2, 3))
+    children, aux = jax.tree_util.tree_flatten(tree2)
+    tree = jax.tree_util.tree_unflatten(aux, children)
+    assert tree.a == 1
+    assert tree.b == MyPyTree(2, 3)
