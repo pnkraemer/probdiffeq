aa4423c15e633a5b035899fab731cf5b267e2558
Nicholas Krämer <nicokraemer@gmx.net>
2023-04-20 17:26:44 +0200

    Explained dataclass choice

--- probdiffeq/backend/containers.py
+++ probdiffeq/backend/containers.py
@@ -2,10 +2,13 @@
 
 import jax_dataclasses
 
-# jax_dataclasses is the only implementation that can handle
-# nested dataclasses properly. We need A LOT OF nested dataclasses.
-# chex.dataclasses did not manage, flax.dataclasses came with some
-# warnings that I did not want to introduce, and tjax required
-# a very recent JAX version, which I found a bit of a harsh
-# dependency restriction for such a small feature.
+# jax_dataclasses is one of few dataclass implementations that can handle
+# nested dataclasses properly.
+# We need A LOT OF nested dataclasses.
+#
+# What about the others?
+# chex.dataclasses requires keyword-only initialisation
+# tjax requires a very recent JAX version (which appears unnecessary for using it here)
+# flax.struct.dataclass works, and jax_dataclasses.pytree_dataclass works.
+# I prefer the jax_dataclasses dependency because it is a much smaller dependency.#
 dataclass_pytree_node = jax_dataclasses.pytree_dataclass
