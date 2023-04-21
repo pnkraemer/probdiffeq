837528cd63155554ae2c739c2601db185c0d054d
Nicholas Krämer <nicokraemer@gmx.net>
2023-04-21 07:15:24 +0200

    Removed custom dataclasses (they don't work)

--- probdiffeq/backend/containers.py
+++ probdiffeq/backend/containers.py
@@ -1,14 +1,5 @@
 """Container types."""
 
-import jax_dataclasses
+import typing
 
-# jax_dataclasses is one of few dataclass implementations that can handle
-# nested dataclasses properly.
-# We need A LOT OF nested dataclasses.
-#
-# What about the others?
-# chex.dataclasses requires keyword-only initialisation
-# tjax requires a very recent JAX version (which appears unnecessary for using it here)
-# flax.struct.dataclass works, and jax_dataclasses.pytree_dataclass works.
-# I prefer the jax_dataclasses dependency because it is a much smaller dependency.#
-dataclass_pytree_node = jax_dataclasses.pytree_dataclass
+NamedTuple = typing.NamedTuple
--- tests/test_backend/__init__.py
+++
@@ -1,1 +0,0 @@
-"""Tests for (very strictly) selected backend functionalities."""
