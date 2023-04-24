82d7f2d7822e05a44f2a4af96c849b7a1e61c003
Nicholas Krämer <nicokraemer@gmx.net>
2023-04-24 17:24:48 +0200

    Remove unused fields from `AdaptiveState` (#602)
    * Split AdaptiveState and RejectionState
    
    * Reshuffled todos

--- probdiffeq/_adaptive.py
+++ probdiffeq/_adaptive.py
@@ -8,15 +8,27 @@ from probdiffeq import controls, ivpsolvers
 from probdiffeq.backend import containers
 
 
-# we could make this generic, but any path there would involve
-# custom pytree-registration, which needs a lot of code for such a simple object.
+# todo: should this move to _collocate?
 class _AdaptiveState(containers.NamedTuple):
-    error_norm_proposed: float
+    """State for adaptive IVP solvers."""
+
     control: Any
-    proposed: Any
     accepted: Any
     solution: Any
     previous: Any
+
+
+class _RejectionState(containers.NamedTuple):
+    """State for rejection loops.
+
+    (Keep decreasing step-size until error norm is small.
+    This is one part of an IVP solver step.)
+    """
+
+    error_norm_proposed: float
+    control: Any
+    proposed: Any
+    step_from: Any
 
 
 def _reference_state_fn_max_abs(sol, sol_previous):
@@ -79,13 +91,8 @@ class AdaptiveIVPSolver(Generic[T]):
         # Initialise the components
         state_solver = self.solver.init(t, posterior, output_scale, num_steps)
         state_control = self.control.init(dt0)
-
-        # Initialise (prototypes for) proposed values
-        error_norm_proposed = _inf_like(0.0)
         return _AdaptiveState(
-            error_norm_proposed=error_norm_proposed,
             solution=state_solver,
-            proposed=state_solver,
             accepted=state_solver,
             previous=state_solver,
             control=state_control,
@@ -116,44 +123,54 @@ class AdaptiveIVPSolver(Generic[T]):
         return state
 
     def _rejection_loop(self, *, vector_field, state0, t1, parameters):
-        def cond_fn(x):
-            proceed_iteration, _ = x
-            return proceed_iteration
+        # todo: this function is sufficiently complex that it should probably
+        #   be extracted from here...
 
-        def body_fn(x):
-            _, s = x
+        def cond_fn(s):
+            return s.error_norm_proposed > 1.0
+
+        def body_fn(s):
+            # _, s = x
             s = self._attempt_step(
                 state=s,
                 vector_field=vector_field,
                 t1=t1,
                 parameters=parameters,
             )
-            proceed_iteration = s.error_norm_proposed > 1.0
-            return proceed_iteration, s
+            return s
 
         def init(s):
-            return True, s
+            larger_than_1 = 1.1
+            st = _RejectionState(
+                error_norm_proposed=larger_than_1,
+                control=s.control,
+                proposed=_inf_like(s.solution),
+                step_from=s.accepted,
+            )
+            return st
 
-        _, state_new = self.while_loop_fn(cond_fn, body_fn, init(state0))
-        return _AdaptiveState(
-            error_norm_proposed=_inf_like(state_new.error_norm_proposed),
-            proposed=_inf_like(state_new.proposed),  # meaningless?
-            accepted=state_new.proposed,  # holla! New! :)
-            solution=state_new.proposed,  # Overwritten by interpolate() if necessary
-            previous=state0.accepted,  # holla! New! :)
-            control=state_new.control,
-        )
+        def extract(s):
+            # 'solution' is overwritten by interpolate() if necessary
+            return _AdaptiveState(
+                accepted=s.proposed,
+                solution=s.proposed,
+                previous=s.step_from,
+                control=s.control,
+            )
 
-    def _attempt_step(self, *, state, vector_field, t1, parameters):
+        state_new = self.while_loop_fn(cond_fn, body_fn, init(state0))
+        return extract(state_new)
+
+    def _attempt_step(self, *, state: _RejectionState, vector_field, t1, parameters):
         """Perform a step with an IVP solver and \
         propose a future time-step based on tolerances and error estimates."""
         # Some controllers like to clip the terminal value instead of interpolating.
         # This must happen _before_ the step.
-        state_control = self.control.clip(state.control, t=state.accepted.t, t1=t1)
+        state_control = self.control.clip(state.control, t=state.step_from.t, t1=t1)
 
         # Perform the actual step.
         state_proposed = self.solver.step(
-            state=state.accepted,
+            state=state.step_from,
             vector_field=vector_field,
             dt=self.control.extract(state_control),
             parameters=parameters,
@@ -161,7 +178,7 @@ class AdaptiveIVPSolver(Generic[T]):
         # Normalise the error and propose a new step.
         error_normalised = self._normalise_error(
             error_estimate=state_proposed.error_estimate,
-            u=self.reference_state_fn(state_proposed.u, state.accepted.u),
+            u=self.reference_state_fn(state_proposed.u, state.step_from.u),
             atol=self.atol,
             rtol=self.rtol,
             norm_ord=self.norm_ord,
@@ -172,13 +189,11 @@ class AdaptiveIVPSolver(Generic[T]):
             error_normalised=error_normalised,
             error_contraction_rate=error_contraction_rate,
         )
-        return _AdaptiveState(
+        return _RejectionState(
             error_norm_proposed=error_normalised,  # new
             proposed=state_proposed,  # new
             control=state_control,  # new
-            solution=state.solution,  # too early to accept
-            accepted=state.accepted,  # too early to accept
-            previous=state.previous,  # too early to accept
+            step_from=state.step_from,
         )
 
     @staticmethod
@@ -192,8 +207,6 @@ class AdaptiveIVPSolver(Generic[T]):
             s0=state.previous, s1=state.accepted, t=t
         )
         return _AdaptiveState(
-            error_norm_proposed=state.error_norm_proposed,
-            proposed=_inf_like(state.proposed),
             accepted=accepted,  # holla! New! :)
             solution=solution,  # holla! New! :)
             previous=previous,  # holla! New! :)
