0f001f0c462850c832efca604f8cd3b1a00a81c0
Nicholas Krämer <nicokraemer@gmx.net>
2023-04-27 11:17:58 +0200

    Fixed more tests

--- tests/test_solution/test_log_marginal_likelihood.py
+++ tests/test_solution/test_log_marginal_likelihood.py
@@ -145,8 +145,8 @@ def test_log_marginal_likelihood_terminal_values_error_for_wrong_shapes(
 def test_filter_ts0_iso_terminal_value_nll(ode_problem, strategy_fn):
     """Issue #477."""
     recipe = recipes.ts0_iso(num_derivatives=4)
-    strategy = strategy_fn(*recipe)
-    solver = ivpsolvers.CalibrationFreeSolver(*strategy)
+    strategy, calibration = strategy_fn(*recipe)
+    solver = ivpsolvers.CalibrationFreeSolver(strategy, calibration)
     sol = ivpsolve.simulate_terminal_values(
         ode_problem.vector_field,
         initial_values=ode_problem.initial_values,
@@ -168,8 +168,8 @@ def test_filter_ts0_iso_terminal_value_nll(ode_problem, strategy_fn):
 def test_nmll_raises_error_for_filter(ode_problem):
     """Non-terminal value calls are not possible for filters."""
     recipe = recipes.ts0_iso(num_derivatives=4)
-    strategy = filters.filter(*recipe)
-    solver = ivpsolvers.CalibrationFreeSolver(*strategy)
+    strategy, calibration = filters.filter(*recipe)
+    solver = ivpsolvers.CalibrationFreeSolver(strategy, calibration)
     grid = jnp.linspace(ode_problem.t0, ode_problem.t1)
 
     sol = ivpsolve.solve_fixed_grid(
