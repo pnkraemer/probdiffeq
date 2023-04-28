d355340381fcefe2c7fa35bed942d3ad4fc06a72
Nicholas Krämer <nicokraemer@gmx.net>
2023-04-28 09:09:35 +0200

    Calibration methods are bundles of init-update-extract schemes (#615)

--- probdiffeq/statespace/iso/calib.py
+++ probdiffeq/statespace/iso/calib.py
@@ -5,8 +5,23 @@ from probdiffeq.statespace import _calib
 
 
 def output_scale():
+    """Construct (a buffet of) isotropic calibration strategies."""
+    mle = _output_scale_mle()
+    dynamic = _output_scale_dynamic()
+    free = _output_scale_free()
+
+    return _calib.CalibrationBundle(mle=mle, dynamic=dynamic, free=free)
+
+
+def _output_scale_mle():
+    """Construct an MLE calibration routine."""
+
     @jax.tree_util.Partial
     def init(s, /):
+        return s
+
+    @jax.tree_util.Partial
+    def update(s, /):  # todo: make correct
         return s
 
     @jax.tree_util.Partial
@@ -15,4 +30,44 @@ def output_scale():
             return s[-1]
         return s
 
-    return _calib.Calib(init=init, extract=extract)
+    return _calib.Calib(init=init, update=update, extract=extract)
+
+
+def _output_scale_dynamic():
+    """Construct a dynamic calibration routine."""
+
+    @jax.tree_util.Partial
+    def init(s, /):
+        return s
+
+    @jax.tree_util.Partial
+    def update(s, /):  # todo: make correct
+        return s
+
+    @jax.tree_util.Partial
+    def extract(s):
+        if s.ndim > 0:
+            return s[-1]
+        return s
+
+    return _calib.Calib(init=init, update=update, extract=extract)
+
+
+def _output_scale_free():
+    """Construct a calibration routine that does not actually do anything."""
+
+    @jax.tree_util.Partial
+    def init(s, /):
+        return s
+
+    @jax.tree_util.Partial
+    def update(s, /):  # todo: make correct
+        return s
+
+    @jax.tree_util.Partial
+    def extract(s):
+        if s.ndim > 0:
+            return s[-1]
+        return s
+
+    return _calib.Calib(init=init, update=update, extract=extract)
