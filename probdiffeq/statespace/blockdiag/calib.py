4687d8d289baaf25446fc9efd133b7cf5c79250b
Nicholas Krämer <nicokraemer@gmx.net>
2023-04-30 09:48:00 +0200

    Call Calib.init() and Calib.extract() in IVP solver (#616)
    * Call Calib.init() and Calib.extract() in IVP solver
    
    * Corrected blockdiag-extract implementation
    
    * Corrected the previous commit

--- probdiffeq/statespace/blockdiag/calib.py
+++ probdiffeq/statespace/blockdiag/calib.py
@@ -10,13 +10,14 @@ def output_scale(output_scale_scalar, *, ode_shape):
     mle = _blockdiag(output_scale_scalar.mle, ode_shape=ode_shape)
     dynamic = _blockdiag(output_scale_scalar.dynamic, ode_shape=ode_shape)
     free = _blockdiag(output_scale_scalar.free, ode_shape=ode_shape)
-
     return _calib.CalibrationBundle(mle=mle, dynamic=dynamic, free=free)
 
 
 def _blockdiag(output_scale_scalar, *, ode_shape):
     @jax.tree_util.Partial
     def init(s, /):
+        # todo: raise warning/error if shape has to be promoted?
+        #  don't just promote by default.
         s_arr = s * jnp.ones(ode_shape)
         return jax.vmap(output_scale_scalar.init)(s_arr)
 
@@ -26,8 +27,12 @@ def _blockdiag(output_scale_scalar, *, ode_shape):
 
     @jax.tree_util.Partial
     def extract(s):
-        if s.ndim > 1:
-            return s[-1, :]
-        return s
+        if jnp.ndim(s) > 1:
+            # For block-diagonal SSMs:
+            # The shape of the solution is (N, d, ...). So we have to vmap along axis=1
+            # This only affects extract() because the other functions assume (N,) = ().
+            return jax.vmap(output_scale_scalar.extract, in_axes=1, out_axes=1)(s)
+
+        return jax.vmap(output_scale_scalar.extract)(s)
 
     return _calib.Calib(init=init, update=update, extract=extract)
