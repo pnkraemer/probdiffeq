e5d3fd87c3fec2f2e56ba188bb682e70dfd59396
Nicholas Krämer <nicokraemer@gmx.net>
2023-08-01 08:41:40 +0200

    Filter methods extracted

--- probdiffeq/strategies/filters.py
+++ probdiffeq/strategies/filters.py
@@ -26,28 +26,55 @@ class _Filter(_strategy.Strategy[_strategy.State, Any]):
     def case_interpolate(
         self, t, *, s0: _strategy.State, s1: _strategy.State, output_scale
     ) -> _interp.InterpRes[_strategy.State]:
-        # A filter interpolates by extrapolating from the previous time-point
-        # to the in-between variable. That's it.
-        dt = t - s0.t
-
-        ssv, extra = self.extrapolation.begin(s0.ssv, s0.extra, dt=dt)
-        ssv, extra = self.extrapolation.complete(ssv, extra, output_scale=output_scale)
-
-        corr_like = jax.tree_util.tree_map(jnp.empty_like, s0.corr)
-        extrapolated = _strategy.State(t=t, ssv=ssv, extra=extra, corr=corr_like)
-        return _interp.InterpRes(
-            accepted=s1, solution=extrapolated, previous=extrapolated
+        return _filter_interpolate(
+            t, output_scale=output_scale, s0=s0, s1=s1, extrapolation=self.extrapolation
         )
 
     def offgrid_marginals(
         self, *, t, marginals, posterior, posterior_previous, t0, t1, output_scale
     ):
-        _acc, sol, _prev = self.case_interpolate(
-            t=t,
-            s1=self.init(t1, posterior),
-            s0=self.init(t0, posterior_previous),
+        return _filter_offgrid_marginals(
+            t,
             output_scale=output_scale,
+            posterior=posterior,
+            posterior_previous=posterior_previous,
+            t0=t0,
+            t1=t1,
+            init=self.init,
+            interpolate=self.case_interpolate,
+            extract=self.extract,
         )
-        t, posterior = self.extract(sol)
-        u = posterior.extract_qoi_from_sample(posterior.mean)
-        return u, posterior
+
+
+def _filter_offgrid_marginals(
+    t,
+    *,
+    output_scale,
+    posterior,
+    posterior_previous,
+    t0,
+    t1,
+    init,
+    interpolate,
+    extract
+):
+    _acc, sol, _prev = interpolate(
+        t=t,
+        s1=init(t1, posterior),
+        s0=init(t0, posterior_previous),
+        output_scale=output_scale,
+    )
+    t, posterior = extract(sol)
+    u = posterior.extract_qoi_from_sample(posterior.mean)
+    return u, posterior
+
+
+def _filter_interpolate(t, *, output_scale, s0, s1, extrapolation):
+    # A filter interpolates by extrapolating from the previous time-point
+    # to the in-between variable. That's it.
+    dt = t - s0.t
+    ssv, extra = extrapolation.begin(s0.ssv, s0.extra, dt=dt)
+    ssv, extra = extrapolation.complete(ssv, extra, output_scale=output_scale)
+    corr_like = jax.tree_util.tree_map(jnp.empty_like, s0.corr)
+    extrapolated = _strategy.State(t=t, ssv=ssv, extra=extra, corr=corr_like)
+    return _interp.InterpRes(accepted=s1, solution=extrapolated, previous=extrapolated)
