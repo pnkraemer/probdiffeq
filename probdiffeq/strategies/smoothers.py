54db2fd8cec01934118a65845d40242026ac55ac
Nicholas Krämer <nicokraemer@gmx.net>
2023-08-01 08:27:41 +0200

    Extracted smoother interpolation

--- probdiffeq/strategies/smoothers.py
+++ probdiffeq/strategies/smoothers.py
@@ -31,43 +31,9 @@ class _Smoother(_strategy.Strategy):
     def case_interpolate(
         self, t, *, s0: _strategy.State, s1: _strategy.State, output_scale
     ) -> _interp.InterpRes[_strategy.State]:
-        """Interpolate.
-
-        A smoother interpolates by_
-        * Extrapolating from t0 to t, which gives the "filtering" marginal
-          and the backward transition from t to t0.
-        * Extrapolating from t to t1, which gives another "filtering" marginal
-          and the backward transition from t1 to t.
-        * Applying the t1-to-t backward transition to compute the interpolation result.
-          This intermediate result is informed about its "right-hand side" datum.
-
-        Subsequent interpolations continue from the value at 't'.
-        Subsequent IVP solver steps continue from the value at 't1'.
-        """
-        # Extrapolate from t0 to t, and from t to t1. This yields all building blocks.
-        e_t = _extrapolate(
-            s0=s0, output_scale=output_scale, t=t, extrapolation=self.extrapolation
+        return _smoother_interpolate(
+            t, s0=s0, s1=s1, output_scale=output_scale, extrapolation=self.extrapolation
         )
-        e_1 = _extrapolate(
-            s0=e_t, output_scale=output_scale, t=s1.t, extrapolation=self.extrapolation
-        )
-
-        # Marginalise from t1 to t to obtain the interpolated solution.
-        # (This function assumes we are in the forward-pass, which is why we interpolate
-        # back from the "filtering" state. In the context of offgrid_marginals,
-        # the backward-interpolation step is repeated from the smoothing marginals)
-        bw_t1_to_t, bw_t_to_t0 = e_1.extra, e_t.extra
-        rv_at_t = bw_t1_to_t.marginalise(s1.ssv.hidden_state)
-        mseq_t = _markov.MarkovSeqRev(init=rv_at_t, conditional=bw_t_to_t0)
-        ssv, _ = self.extrapolation.init(mseq_t)
-        corr_like = jax.tree_util.tree_map(jnp.empty_like, s1.corr)
-        state_at_t = _strategy.State(t=t, ssv=ssv, corr=corr_like, extra=bw_t_to_t0)
-
-        # The state at t1 gets a new backward model; it must remember how to
-        # get back to t, not to t0.
-        # The other two are the extrapolated solution
-        s_1 = _strategy.State(t=s1.t, ssv=s1.ssv, corr=s1.corr, extra=bw_t1_to_t)
-        return _interp.InterpRes(accepted=s_1, solution=state_at_t, previous=state_at_t)
 
     def offgrid_marginals(
         self,
@@ -90,14 +56,6 @@ class _Smoother(_strategy.Strategy):
         marginals = posterior.conditional.marginalise(marginals)
         u = marginals.extract_qoi_from_sample(marginals.mean)
         return u, marginals
-
-
-def _extrapolate(s0, output_scale, t, *, extrapolation):
-    dt = t - s0.t
-    ssv, extra = extrapolation.begin(s0.ssv, s0.extra, dt=dt)
-    ssv, extra = extrapolation.complete(ssv, extra, output_scale=output_scale)
-    corr_like = jax.tree_util.tree_map(jnp.empty_like, s0.corr)
-    return _strategy.State(t=t, ssv=ssv, extra=extra, corr=corr_like)
 
 
 @jax.tree_util.register_pytree_node_class
@@ -198,3 +156,50 @@ class _FixedPointSmoother(_strategy.Strategy):
 
         # Bundle up the results and return
         return _interp.InterpRes(accepted=acc_t1, solution=sol_t, previous=prev_t)
+
+
+# todo: state_t0 and state_t1 variable names
+def _smoother_interpolate(t, *, s0, s1, output_scale, extrapolation):
+    """Interpolate.
+
+    A smoother interpolates by_
+    * Extrapolating from t0 to t, which gives the "filtering" marginal
+      and the backward transition from t to t0.
+    * Extrapolating from t to t1, which gives another "filtering" marginal
+      and the backward transition from t1 to t.
+    * Applying the t1-to-t backward transition to compute the interpolation result.
+      This intermediate result is informed about its "right-hand side" datum.
+
+    Subsequent interpolations continue from the value at 't'.
+    Subsequent IVP solver steps continue from the value at 't1'.
+    """
+    # Extrapolate from t0 to t, and from t to t1. This yields all building blocks.
+    e_t = _extrapolate(
+        s0=s0, output_scale=output_scale, t=t, extrapolation=extrapolation
+    )
+    e_1 = _extrapolate(
+        s0=e_t, output_scale=output_scale, t=s1.t, extrapolation=extrapolation
+    )
+    # Marginalise from t1 to t to obtain the interpolated solution.
+    # (This function assumes we are in the forward-pass, which is why we interpolate
+    # back from the "filtering" state. In the context of offgrid_marginals,
+    # the backward-interpolation step is repeated from the smoothing marginals)
+    bw_t1_to_t, bw_t_to_t0 = e_1.extra, e_t.extra
+    rv_at_t = bw_t1_to_t.marginalise(s1.ssv.hidden_state)
+    mseq_t = _markov.MarkovSeqRev(init=rv_at_t, conditional=bw_t_to_t0)
+    ssv, _ = extrapolation.init(mseq_t)
+    corr_like = jax.tree_util.tree_map(jnp.empty_like, s1.corr)
+    state_at_t = _strategy.State(t=t, ssv=ssv, corr=corr_like, extra=bw_t_to_t0)
+    # The state at t1 gets a new backward model; it must remember how to
+    # get back to t, not to t0.
+    # The other two are the extrapolated solution
+    s_1 = _strategy.State(t=s1.t, ssv=s1.ssv, corr=s1.corr, extra=bw_t1_to_t)
+    return _interp.InterpRes(accepted=s_1, solution=state_at_t, previous=state_at_t)
+
+
+def _extrapolate(s0, output_scale, t, *, extrapolation):
+    dt = t - s0.t
+    ssv, extra = extrapolation.begin(s0.ssv, s0.extra, dt=dt)
+    ssv, extra = extrapolation.complete(ssv, extra, output_scale=output_scale)
+    corr_like = jax.tree_util.tree_map(jnp.empty_like, s0.corr)
+    return _strategy.State(t=t, ssv=ssv, extra=extra, corr=corr_like)
