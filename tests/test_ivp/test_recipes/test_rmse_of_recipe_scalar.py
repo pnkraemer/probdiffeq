b8733722ebd68a31a4c0ac4007fe2ccc3a9ea822
Nicholas Krämer <nicokraemer@gmx.net>
2023-08-05 16:41:28 +0200

    Tests for scalar smoothers

--- tests/test_ivp/test_recipes/test_rmse_of_recipe_scalar.py
+++ tests/test_ivp/test_recipes/test_rmse_of_recipe_scalar.py
@@ -8,6 +8,7 @@ import jax.numpy as jnp
 from probdiffeq import ivpsolve, test_util
 from probdiffeq.backend import testing
 from probdiffeq.statespace import recipes
+from probdiffeq.strategies import filters, smoothers
 
 
 @testing.fixture(name="problem")
@@ -22,19 +23,31 @@ def fixture_problem():
 
 
 @testing.case()
-def case_ts0_scalar():
-    return recipes.ts0_scalar, 1.0
+def case_filter():
+    return filters.filter
+
+
+@testing.case()
+def case_smoother():
+    return smoothers.smoother
+
+
+@testing.case()
+def case_fixedpoint():
+    return smoothers.smoother_fixedpoint
 
 
 @testing.fixture(name="solution")
-@testing.parametrize_with_cases("factorisation", cases=".", prefix="case_")
-def fixture_recipe_solution(problem, factorisation):
+@testing.parametrize_with_cases("strategy", cases=".", prefix="case_")
+def fixture_recipe_solution(problem, strategy):
     vf, u0, (t0, t1), f_args = problem
 
     problem_args = (vf, (u0,))
     problem_kwargs = {"t0": t0, "t1": t1, "parameters": f_args}
-    impl_factory, output_scale = factorisation
-    solver = test_util.generate_solver(num_derivatives=2, impl_factory=impl_factory)
+    impl_factory, output_scale = recipes.ts0_scalar, 1.0
+    solver = test_util.generate_solver(
+        num_derivatives=2, impl_factory=impl_factory, strategy_factory=strategy
+    )
     adaptive_kwargs = {
         "solver": solver,
         "atol": 1e-2,
