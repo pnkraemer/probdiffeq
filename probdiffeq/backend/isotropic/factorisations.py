b6396799c1da9f86b52f96faebd3d6093b31acdc
Nicholas Krämer <nicokraemer@gmx.net>
2023-08-07 09:02:37 +0200

    Created iso subpackage

--- probdiffeq/backend/isotropic/factorisations.py
+++ probdiffeq/backend/isotropic/factorisations.py
@@ -1,5 +1,19 @@
 from probdiffeq.backend import _factorisations
+from probdiffeq.backend.isotropic import conditional, linearise, random, ssm_util
 
 
 class IsotropicFactorisation(_factorisations.Factorisation):
-    pass
+    def conditional(self):
+        raise NotImplementedError
+
+    def linearise_ode(self):
+        return linearise.LineariseODEBackEnd()
+
+    def random(self):
+        raise NotImplementedError
+
+    def ssm_util(self):
+        raise NotImplementedError
+
+    def transform(self):
+        raise NotImplementedError
---
+++ probdiffeq/backend/isotropic/linearise.py
@@ -0,0 +1,19 @@
+from probdiffeq.backend import _linearise
+
+
+class LineariseODEBackEnd(_linearise.LineariseODEBackEnd):
+    def constraint_1st(self, ode_order):
+        raise NotImplementedError
+
+    def constraint_0th(self, ode_order):
+        def linearise_fun_wrapped(fun, mean):
+            fx = ts0(fun, mean[:ode_order, ...])
+            return lambda s: s[ode_order, ...], -fx
+
+        return linearise_fun_wrapped
+
+    def constraint_statistical_0th(self, cubature_fun):
+        raise NotImplementedError
+
+    def constraint_statistical_1st(self, cubature_fun):
+        raise NotImplementedError
