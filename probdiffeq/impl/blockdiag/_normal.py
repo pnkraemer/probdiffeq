1be76ae2a95e5962df7eb49d0f5a229e653cb444
Nicholas Krämer <nicokraemer@gmx.net>
2023-08-08 11:37:28 +0200

    Better shape-checks in blockdiag

--- probdiffeq/impl/blockdiag/_normal.py
+++ probdiffeq/impl/blockdiag/_normal.py
@@ -4,12 +4,18 @@ import jax.numpy as jnp
 
 class Normal:
     def __init__(self, mean, cholesky):
-        assert jnp.ndim(mean) >= 2, jnp.shape(mean)
-        assert jnp.ndim(cholesky) >= 3, jnp.shape(cholesky)
+        # Only shape-check if the inputs are arrays.
+        # When debugging, for example, mean and cholesky may be shapes or ndims
+        if isinstance(mean, jax.Array):
+            assert isinstance(cholesky, jax.Array)
+            assert jnp.ndim(mean) >= 2, jnp.shape(mean)
+            assert jnp.ndim(cholesky) >= 3, jnp.shape(cholesky)
 
-        *_, d1, n1 = jnp.shape(mean)
-        *_, d2, n2, n3 = jnp.shape(cholesky)
-        assert d1 == d2 and n1 == n2 == n3, (jnp.shape(mean), jnp.shape(cholesky))
+            *b1, d1, n1 = jnp.shape(mean)
+            *b2, d2, n2, n3 = jnp.shape(cholesky)
+
+            print_if_fails = (jnp.shape(mean), jnp.shape(cholesky))
+            assert b1 == b2 and d1 == d2 and n1 == n2 == n3, print_if_fails
 
         self.mean = mean
         self.cholesky = cholesky
@@ -22,7 +28,7 @@ def _flatten(normal):
 
 
 def _unflatten(_aux, children):
-    mean, cholesky = children
+    (mean, cholesky) = children
     return Normal(mean, cholesky)
 
 
