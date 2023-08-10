f240e50e73f7c9a4fcc7162a56c75b9af9193605
Nicholas Krämer <nicokraemer@gmx.net>
2023-08-10 16:27:37 +0200

    FIxed offgrid-marginals test

--- tests/test_solvers/test_solution/test_offgrid_marginals.py
+++ tests/test_solvers/test_solution/test_offgrid_marginals.py
@@ -1,99 +1,61 @@
 """Tests for IVP solvers."""
-import diffeqzoo.ivps
-import jax
 import jax.numpy as jnp
 
 from probdiffeq import ivpsolve
-from probdiffeq.backend import testing
-from probdiffeq.solvers import solution
-from probdiffeq.solvers.statespace import recipes
+from probdiffeq.impl import impl
+from probdiffeq.solvers import solution, uncalibrated
+from probdiffeq.solvers.statespace import correction, extrapolation
 from probdiffeq.solvers.strategies import filters, smoothers
-from probdiffeq.util import test_util
+from tests.setup import setup
 
 
-@testing.fixture(name="problem")
-def fixture_problem():
-    f, u0, (t0, _), f_args = diffeqzoo.ivps.lotka_volterra()
-    t1 = 2.0  # Short time-intervals are sufficient for this test.
-
-    @jax.jit
-    def vf(x, *, t, p):  # noqa: ARG001
-        return f(x, *p)
-
-    return vf, u0, (t0, t1), f_args
-
-
-@testing.case()
-def case_isotropic_factorisation():
-    return recipes.ts0_iso, 2.0
-
-
-@testing.case()  # this implies success of the scalar solver
-def case_blockdiag_factorisation():
-    return recipes.ts0_blockdiag, jnp.ones((2,)) * 2.0
-
-
-@testing.case()
-def case_dense_factorisation():
-    return recipes.ts0_dense, 2.0
-
-
-@testing.parametrize_with_cases("factorisation", cases=".", prefix="case_")
-def test_filter_marginals_close_only_to_left_boundary(problem, factorisation):
+def test_filter_marginals_close_only_to_left_boundary():
     """Assert that the filter-marginals interpolate well close to the left boundary."""
-    vf, u0, (t0, t1), f_args = problem
-    impl_factory, output_scale = factorisation
-    solver = test_util.generate_solver(
-        num_derivatives=1,
-        impl_factory=impl_factory,
-        strategy_factory=filters.filter,
-        ode_shape=jnp.shape(u0),
-    )
-    sol = ivpsolve.solve_with_python_while_loop(
+    vf, (u0,), (t0, t1) = setup.ode()
+
+    ibm = extrapolation.ibm_adaptive(num_derivatives=1)
+    ts0 = correction.taylor_order_zero()
+    strategy = filters.filter_adaptive(ibm, ts0)
+    solver = uncalibrated.solver(strategy)
+
+    output_scale = jnp.ones_like(impl.ssm_util.prototype_output_scale())
+    sol = ivpsolve.solve_fixed_grid(
         vf,
         (u0,),
-        t0=t0,
-        t1=t1,
-        parameters=f_args,
+        grid=jnp.linspace(t0, t1, endpoint=True, num=5),
         solver=solver,
         output_scale=output_scale,
-        atol=1e-2,
-        rtol=1e-2,
     )
 
     # Extrapolate from the left: close-to-left boundary must be similar,
     # but close-to-right boundary needs not be similar
-    ts = jnp.linspace(t0 + 1e-4, t1 - 1e-4, num=4, endpoint=True)
+    ts = jnp.linspace(sol.t[-2] + 1e-4, sol.t[-1] - 1e-4, num=5, endpoint=True)
     u, _ = solution.offgrid_marginals_searchsorted(ts=ts, solution=sol, solver=solver)
-    assert jnp.allclose(u[0], sol.u[0], atol=1e-3, rtol=1e-3)
-    assert not jnp.allclose(u[0], sol.u[1], atol=1e-3, rtol=1e-3)
+    assert jnp.allclose(u[0], sol.u[-2], atol=1e-3, rtol=1e-3)
+    assert not jnp.allclose(u[-1], sol.u[-1], atol=1e-3, rtol=1e-3)
 
 
-@testing.parametrize_with_cases("factorisation", cases=".", prefix="case_")
-def test_smoother_marginals_close_to_both_boundaries(problem, factorisation):
+def test_smoother_marginals_close_to_both_boundaries():
     """Assert that the smoother-marginals interpolate well close to the boundary."""
-    vf, u0, (t0, t1), f_args = problem
-    impl_factory, output_scale = factorisation
-    solver = test_util.generate_solver(
-        num_derivatives=1,
-        impl_factory=impl_factory,
-        strategy_factory=smoothers.smoother_adaptive,
-        ode_shape=jnp.shape(u0),
-    )
-    sol = ivpsolve.solve_with_python_while_loop(
+    vf, (u0,), (t0, t1) = setup.ode()
+
+    ibm = extrapolation.ibm_adaptive(num_derivatives=4)
+    ts0 = correction.taylor_order_zero()
+    strategy = smoothers.smoother_adaptive(ibm, ts0)
+    solver = uncalibrated.solver(strategy)
+
+    output_scale = jnp.ones_like(impl.ssm_util.prototype_output_scale())
+    sol = ivpsolve.solve_fixed_grid(
         vf,
         (u0,),
-        t0=t0,
-        t1=t1,
-        parameters=f_args,
+        grid=jnp.linspace(t0, t1, endpoint=True, num=5),
         solver=solver,
         output_scale=output_scale,
-        atol=1e-2,
-        rtol=1e-2,
     )
     # Extrapolate from the left: close-to-left boundary must be similar,
-    # but close-to-right boundary must not be similar
-    ts = jnp.linspace(t0 + 1e-4, t1 - 1e-4, num=4, endpoint=True)
+    # and close-to-right boundary must be similar
+    ts = jnp.linspace(sol.t[-2] + 1e-4, sol.t[-1] - 1e-4, num=5, endpoint=True)
     u, _ = solution.offgrid_marginals_searchsorted(ts=ts, solution=sol, solver=solver)
-    assert jnp.allclose(u[0], sol.u[0], atol=1e-3, rtol=1e-3)
+
+    assert jnp.allclose(u[0], sol.u[-2], atol=1e-3, rtol=1e-3)
     assert jnp.allclose(u[-1], sol.u[-1], atol=1e-3, rtol=1e-3)
