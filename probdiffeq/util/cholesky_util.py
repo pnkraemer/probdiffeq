Fix Cholesky-utility-gradients for zero covariances

probdiffeq/util/cholesky_util.py
@@ -48,7 +48,6 @@ def revert_conditional_noisefree(R_X_F, R_X):
     return r_marg, (r_cor, gain)
 
 
-# rename: reparametrise_conditional_correlation?
 def revert_conditional(R_X_F, R_X, R_YX):
     r"""Revert the  square-root correlation in a Gaussian transition kernel.
 
@@ -95,7 +94,11 @@ def revert_conditional(R_X_F, R_X, R_YX):
             [R_X_F, R_X],
         ]
     )
-    R = triu_via_qr(R)
+    # If R_X_F and R_X are zero, but R_YX is not zero, qr(R) can be implemented via
+    # qr(R_YX) and embedding the result in zeros. This is what we do here.
+    # Without this case distinction, reverse-mode derivatives are not defined.
+    cond = jnp.logical_and(jnp.linalg.norm(R_X_F) == 0.0, jnp.linalg.norm(R_X) == 0.0)
+    R = jax.lax.cond(cond, _triu_via_shortcut, lambda a, _b: triu_via_qr(a), R, R_YX)
 
     # ~R_{Y}
     d_out = R_YX.shape[1]
@@ -110,6 +113,20 @@ def revert_conditional(R_X_F, R_X, R_YX):
     # ~R_{X \mid Y}
     R_XY = R[d_out:, d_out:]
     return R_Y, (R_XY, G)
+
+
+def _triu_via_shortcut(R, R_YX):
+    """If R_X and R_X_F are zero, triu_via_qr(R) can be implemented cheaply.
+
+    Namely, by applying it only to R_YX and embedding the result in zeros.
+    This is not only more efficient because it requires fewer floating-point operations
+    than qr-decomposing the full matrix, but it also admits a well-defined
+    reverse-mode derivative!
+    """
+    R = jnp.zeros_like(R)
+    R_YX = triu_via_qr(R_YX)
+    n, m = jnp.shape(R_YX)
+    return R.at[:n, :m].set(R_YX)
 
 
 def _is_matrix(mat, matrix_ndim=2):
@@ -146,4 +163,16 @@ def triu_via_qr(R, /):
     # TODO: enforce positive diagonals?
     #  (or expose this option; some equivalence tests might fail
     #   if we always use a positive diagonal.)
-    return jnp.linalg.qr(R, mode="r")
+
+    # Only do something if R is not already triangular.
+    # This distinction may seem unnecessary but is important,
+    # because otherwise, reverse-mode derivatives are not defined!
+    matrix_is_already_triu = jnp.all(jnp.triu(R) == R)
+
+    _nrows, ncols = jnp.shape(R)
+    return jax.lax.cond(
+        matrix_is_already_triu,
+        lambda s: s[:ncols, :ncols],
+        lambda s: jnp.linalg.qr(s, mode="R"),
+        R,
+    )

tests/test_util/test_cholesky_util.py
@@ -4,6 +4,7 @@ These are so crucial and annoying to debug that they need their own test set.
 """
 from math import prod
 
+import jax
 import jax.numpy as jnp
 
 from probdiffeq.backend import testing
@@ -13,7 +14,7 @@ _SHAPES = ([(4, 3), (3, 3), (4, 4)], [(2, 3), (3, 3), (2, 2)])
 
 
 @testing.parametrize("HCshape, Cshape, Xshape", _SHAPES)
-def test_revert_kernel_scalar(HCshape, Cshape, Xshape):
+def test_revert_conditional(HCshape, Cshape, Xshape):
     HC = _some_array(HCshape) + 1.0
     C = _some_array(Cshape) + 2.0
     X = _some_array(Xshape) + 3.0 + jnp.eye(*Xshape)
@@ -74,3 +75,38 @@ def test_sqrt_sum_square_error():
     c = 5.0 * jnp.eye(2)
     with testing.raises(ValueError, match="scalar"):
         _ = cholesky_util.sqrt_sum_square_scalar(a, b, c)
+
+
+def test_reverse_conditional_jacrev_zero_matrix():
+    """For zero-valued input covariances, reverse-mode gradients need a trick.
+
+    This resolves issue 668.
+    """
+    C = _some_array((3, 3)) * 0.0
+    HC = _some_array((2, 3)) * 0.0
+    X = _some_array((2, 2)) + 3.0 + jnp.eye(2)
+
+    result = jax.jacrev(cholesky_util.revert_conditional)(HC.T, C.T, X.T)
+    is_not_nan = _tree_is_free_of_nans(result)
+    assert is_not_nan
+
+
+def test_sum_of_sqrtm_factors_jacrev_zero_matrix():
+    """For zero-valued input covariances, reverse-mode gradients need a trick.
+
+    This resolves issue 668.
+    """
+    C = _some_array((3, 3)) * 0.0
+    HC = _some_array((3, 2))
+
+    result = jax.jacrev(cholesky_util.sum_of_sqrtm_factors)((C.T, HC.T))
+    is_not_nan = _tree_is_free_of_nans(result)
+    assert is_not_nan
+
+
+def _tree_is_free_of_nans(tree):
+    def contains_no_nan(x):
+        return jnp.logical_not(jnp.any(jnp.isnan(x)))
+
+    tree_contains_no_nan = jax.tree_util.tree_map(contains_no_nan, tree)
+    return jax.tree_util.tree_all(tree_contains_no_nan)