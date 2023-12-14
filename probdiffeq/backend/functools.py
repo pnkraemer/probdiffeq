be2980433ebe32224ae1273341b5692d1768941e
Nicholas Krämer <nicokraemer@gmx.net>
2023-12-14 17:50:12 +0100

    Implement backend.functools.jet

--- probdiffeq/backend/functools.py
+++ probdiffeq/backend/functools.py
@@ -2,6 +2,7 @@
 import functools
 
 import jax
+import jax.experimental.jet
 
 
 def vmap(func, /, in_axes=0, out_axes=0):
@@ -14,3 +15,7 @@ def partial(func, *args, **kwargs):
 
 def jit(func, /, static_argnums=None, static_argnames=None):
     return jax.jit(func, static_argnums=static_argnums, static_argnames=static_argnames)
+
+
+def jet(func, /, primals, series):
+    return jax.experimental.jet.jet(func, primals=primals, series=series)
