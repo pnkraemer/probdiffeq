727103160511f7b413f34ccf2d5c3c57de764a9f
Nicholas Krämer <nicokraemer@gmx.net>
2023-12-15 10:57:47 +0100

    Remove unused imports from taylor

--- probdiffeq/taylor/estim.py
+++ probdiffeq/taylor/estim.py
@@ -2,7 +2,6 @@ r"""Taylor-expand the solution of an initial value problem (IVP)."""
 
 
 import jax
-import jax.experimental.jet
 import jax.experimental.ode
 
 from probdiffeq.backend import functools
