fd780246537d04af9e45a705a3e014c75200cb2d
Nicholas Krämer <nicokraemer@gmx.net>
2023-12-28 12:26:46 +0100

    Implement backend.stats and backend.functools.jacrev

--- tests/test_impl/test_logpdfs.py
+++ tests/test_impl/test_logpdfs.py
@@ -3,8 +3,8 @@
 Necessary because the implementation has been faulty in the past. Never again.
 """
 
-import jax.scipy.stats
 
+from probdiffeq.backend import functools, stats
 from probdiffeq.backend import numpy as np
 from probdiffeq.impl import impl
 from tests.setup import setup
@@ -18,7 +18,7 @@ def test_logpdf():
     u_dense = np.ones_like(mean_dense)
 
     pdf1 = impl.stats.logpdf(u, rv)
-    pdf2 = jax.scipy.stats.multivariate_normal.logpdf(u_dense, mean_dense, cov_dense)
+    pdf2 = stats.multivariate_normal_logpdf(u_dense, mean_dense, cov_dense)
     assert np.allclose(pdf1, pdf2)
 
 
@@ -26,6 +26,6 @@ def test_grad_not_none():
     rv = setup.rv()
     u = np.ones_like(impl.stats.mean(rv))
 
-    pdf = jax.jacrev(impl.stats.logpdf)(u, rv)
+    pdf = functools.jacrev(impl.stats.logpdf)(u, rv)
     assert not np.any(np.isinf(pdf))
     assert not np.any(np.isnan(pdf))
