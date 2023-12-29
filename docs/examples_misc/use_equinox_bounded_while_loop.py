29ec475884150dad20406eac12381688a73de13f
Nicholas Krämer <nicokraemer@gmx.net>
2023-12-29 13:24:22 +0100

    Implement backend.control_flow.context_overwrite_{while_loop, scan} and create a tutorial for using it (#736)
    * Explain direct jax.import outside toplevel
    
    * Implement backend.control_flow.update_while_loop_func
    
    * Update the tests
    
    * Rename bounded-while-loop tutorial
    
    * Change overwrite_{scan, while_loop}_func implementation to a context manager
    
    * Update example to use the context manager
    
    * Rename a variable in the reverse-mode notebook
    
    * Rename control_flow.overwrite_*_func functions to control_flow.context_overwrite_*

---
+++ docs/examples_misc/use_equinox_bounded_while_loop.py
@@ -0,0 +1,106 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.15.2
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# # Use Equinox's reverse-mode differentiable while-loops
+#
+# Use [Equinox's](https://docs.kidger.site/equinox/)
+# bounded while loop to enable reverse-mode differentiation of adaptive IVP solvers.
+
+# +
+"""Use Equinox's while loop to compute gradients of `simulate_terminal_values`."""
+
+import equinox
+import jax
+import jax.config
+import jax.numpy as jnp
+
+from probdiffeq import adaptive, ivpsolve
+from probdiffeq.backend import control_flow
+from probdiffeq.impl import impl
+from probdiffeq.solvers import uncalibrated
+from probdiffeq.solvers.strategies import fixedpoint
+from probdiffeq.solvers.strategies.components import corrections, priors
+from probdiffeq.taylor import autodiff
+
+jax.config.update("jax_platform_name", "cpu")
+impl.select("dense", ode_shape=(1,))
+
+
+# -
+
+# Overwrite the while-loop (via a context manager):
+
+
+# +
+def while_loop_func(*a, **kw):
+    """Evaluate a bounded while loop."""
+    return equinox.internal.while_loop(*a, **kw, kind="bounded", max_steps=100)
+
+
+context_compute_gradient = control_flow.context_overwrite_while_loop(while_loop_func)
+# -
+
+# The rest is the similar to the "easy example" in the quickstart,
+# except for simulating adaptively and
+# computing the value and the gradient
+# (which is impossible without the specialised while-loop implementation).
+
+
+def solution_routine():
+    """Construct a parameter-to-solution function and an initial value."""
+
+    def vf(y, *, t):  # noqa: ARG001
+        """Evaluate the vector field."""
+        return 0.5 * y * (1 - y)
+
+    t0, t1 = 0.0, 1.0
+    u0 = jnp.asarray([0.1])
+
+    ibm = priors.ibm_adaptive(num_derivatives=1)
+    ts0 = corrections.ts0(ode_order=1)
+
+    strategy = fixedpoint.fixedpoint_adaptive(ibm, ts0)
+    solver = uncalibrated.solver(strategy)
+    adaptive_solver = adaptive.adaptive(solver)
+
+    tcoeffs = autodiff.taylor_mode_scan(lambda y: vf(y, t=t0), (u0,), num=1)
+    init = solver.initial_condition(tcoeffs, 1.0)
+
+    def simulate(init_val):
+        """Evaluate the parameter-to-solution function."""
+        sol = ivpsolve.simulate_terminal_values(
+            vf, init_val, t0=t0, t1=t1, dt0=0.1, adaptive_solver=adaptive_solver
+        )
+
+        # Any scalar function of the IVP solution would do
+        return jnp.dot(sol.u, sol.u)
+
+    return simulate, init
+
+
+try:
+    solve, x = solution_routine()
+    solution, gradient = jax.value_and_grad(solve)(x)
+except ValueError as err:
+    print(f"Caught error:\n\t {err}")
+
+with context_compute_gradient:
+    # Construct the solution routine inside the context
+    solve, x = solution_routine()
+
+    # Compute gradients
+    solution, gradient = jax.value_and_grad(solve)(x)
+
+    print(solution)
+    print(gradient)
--- probdiffeq/backend/control_flow.py
+++ probdiffeq/backend/control_flow.py
@@ -1,14 +1,57 @@
 """Control-flow wrappers."""
 
+import contextlib
+
 import jax
 
+_jax_scan = jax.lax.scan
+_jax_while_loop = jax.lax.while_loop
 
-def while_loop(cond_func, body_func, /, init):
-    return jax.lax.while_loop(cond_fun=cond_func, body_fun=body_func, init_val=init)
+
+@contextlib.contextmanager
+def context_overwrite_scan(func, /):
+    """Overwrite the scan() function.
+
+    Parameters
+    ----------
+    func:
+        A function with the same signature as `jax.lax.scan`.
+    """
+    global _jax_scan
+    tmp = _jax_scan
+
+    try:
+        _jax_scan = func
+        yield _jax_scan
+    finally:
+        _jax_scan = tmp
 
 
 def scan(step_func, /, init, xs, *, reverse=False, length=None):
-    return jax.lax.scan(step_func, init=init, xs=xs, reverse=reverse, length=length)
+    return _jax_scan(step_func, init=init, xs=xs, reverse=reverse, length=length)
+
+
+@contextlib.contextmanager
+def context_overwrite_while_loop(func, /):
+    """Overwrite the while_loop() function.
+
+    Parameters
+    ----------
+    func:
+        A function with the same signature as `jax.lax.while_loop`.
+    """
+    global _jax_while_loop
+    tmp = _jax_while_loop
+
+    try:
+        _jax_while_loop = func
+        yield _jax_while_loop
+    finally:
+        _jax_while_loop = tmp
+
+
+def while_loop(cond_func, body_func, /, init):
+    return _jax_while_loop(cond_fun=cond_func, body_fun=body_func, init_val=init)
 
 
 def cond(use_true_func, true_func, false_func, *operands):
