f1a9c91b8e0ab7585bdbce92db812a3c077329a0
Nicholas Krämer <nicokraemer@gmx.net>
2024-06-17 08:20:25 +0200

    Move IsotropicLinearisation

--- probdiffeq/impl/_linearise.py
+++ probdiffeq/impl/_linearise.py
@@ -193,3 +193,26 @@ class DenseLinearisation(LinearisationBackend):
         cov_sqrtm = cholesky_util.triu_via_qr(fx_centered_normed)
 
         return _normal.Normal(fx_mean, cov_sqrtm.T)
+
+
+class IsotropicLinearisation(LinearisationBackend):
+    def ode_taylor_1st(self, ode_order):
+        raise NotImplementedError
+
+    def ode_taylor_0th(self, ode_order):
+        def linearise_fun_wrapped(fun, mean):
+            fx = self.ts0(fun, mean[:ode_order, ...])
+            linop = linop_util.parametrised_linop(lambda s, _p: s[[ode_order], ...])
+            return linop, -fx
+
+        return linearise_fun_wrapped
+
+    def ode_statistical_0th(self, cubature_fun):
+        raise NotImplementedError
+
+    def ode_statistical_1st(self, cubature_fun):
+        raise NotImplementedError
+
+    @staticmethod
+    def ts0(fn, m):
+        return fn(m)
