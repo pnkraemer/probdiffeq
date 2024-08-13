7515910c0a4bc712b584aef2357249d1b6abbed0
Nicholas Krämer <nicokraemer@gmx.net>
2024-08-13 09:58:54 +0200

    Make the correction-constructors functional to match the other constructors

--- probdiffeq/ivpsolvers.py
+++ probdiffeq/ivpsolvers.py
@@ -661,99 +661,134 @@ def prior_ibm_discrete(ts, *, num_derivatives, output_scale=None):
     return stats.MarkovSeq(init, conditionals)
 
 
-class _Correction(abc.ABC):
+@containers.dataclass
+class _Correction:
     """Correction model interface."""
 
-    def __init__(self, ode_order):
-        self.ode_order = ode_order
+    name: str
+    ode_order: int
 
-    @abc.abstractmethod
-    def init(self, x, /):
-        """Initialise the state from the solution."""
-        raise NotImplementedError
+    init: Callable
+    """Initialise the state from the solution."""
 
-    @abc.abstractmethod
-    def estimate_error(self, ssv, corr, /, vector_field, t):
-        """Perform all elements of the correction until the error estimate."""
-        raise NotImplementedError
+    estimate_error: Callable
+    """Perform all elements of the correction until the error estimate."""
 
-    @abc.abstractmethod
-    def complete(self, ssv, corr, /):
-        """Complete what has been left out by `estimate_error`."""
-        raise NotImplementedError
+    complete: Callable
+    """Complete what has been left out by `estimate_error`."""
 
-    @abc.abstractmethod
-    def extract(self, ssv, corr, /):
-        """Extract the solution from the state."""
-        raise NotImplementedError
+    extract: Callable
+    """Extract the solution from the state."""
 
 
-class _ODEConstraintTaylor(_Correction):
-    def __init__(self, ode_order, linearise_fun, string_repr):
-        super().__init__(ode_order=ode_order)
+def correction_ts0(*, ode_order=1) -> _Correction:
+    """Zeroth-order Taylor linearisation."""
+    return _correction_constraint_ode_taylor(
+        ode_order=ode_order,
+        linearise_fun=impl.linearise.ode_taylor_0th(ode_order=ode_order),
+        string_repr=f"<TS0 with ode_order={ode_order}>",
+    )
 
-        self.linearise = linearise_fun
-        self.string_repr = string_repr
 
-    def __repr__(self):
-        return self.string_repr
+def correction_ts1(*, ode_order=1) -> _Correction:
+    """First-order Taylor linearisation."""
+    return _correction_constraint_ode_taylor(
+        ode_order=ode_order,
+        linearise_fun=impl.linearise.ode_taylor_1st(ode_order=ode_order),
+        string_repr=f"<TS1 with ode_order={ode_order}>",
+    )
 
-    def init(self, ssv, /):
+
+def _correction_constraint_ode_taylor(
+    ode_order, linearise_fun, string_repr
+) -> _Correction:
+    def init(ssv, /):
         obs_like = impl.prototypes.observed()
         return ssv, obs_like
 
-    def estimate_error(self, hidden_state, _corr, /, vector_field, t):
+    def estimate_error(hidden_state, _corr, /, vector_field, t):
         def f_wrapped(s):
             return vector_field(*s, t=t)
 
-        A, b = self.linearise(f_wrapped, hidden_state.mean)
+        A, b = linearise_fun(f_wrapped, hidden_state.mean)
         observed = impl.transform.marginalise(hidden_state, (A, b))
 
         error_estimate = _estimate_error(observed)
         return error_estimate, observed, (A, b)
 
-    def complete(self, hidden_state, corr, /):
+    def complete(hidden_state, corr, /):
         A, b = corr
         observed, (_gain, corrected) = impl.transform.revert(hidden_state, (A, b))
         return corrected, observed
 
-    def extract(self, ssv, _corr, /):
+    def extract(ssv, _corr, /):
         return ssv
 
+    return _Correction(
+        ode_order=ode_order,
+        name=string_repr,
+        init=init,
+        estimate_error=estimate_error,
+        complete=complete,
+        extract=extract,
+    )
 
-class _ODEConstraintStatistical(_Correction):
-    def __init__(self, ode_order, linearise_fun, string_repr):
-        super().__init__(ode_order=ode_order)
 
-        self.linearise = linearise_fun
-        self.string_repr = string_repr
+def correction_slr0(cubature_fun=cubature_third_order_spherical) -> _Correction:
+    """Zeroth-order statistical linear regression."""
+    linearise_fun = impl.linearise.ode_statistical_1st(cubature_fun)
+    return _correction_constraint_ode_statistical(
+        ode_order=1,
+        linearise_fun=linearise_fun,
+        string_repr=f"<SLR1 with ode_order={1}>",
+    )
 
-    def __repr__(self):
-        return self.string_repr
 
-    def init(self, ssv, /):
+def correction_slr1(cubature_fun=cubature_third_order_spherical) -> _Correction:
+    """First-order statistical linear regression."""
+    linearise_fun = impl.linearise.ode_statistical_0th(cubature_fun)
+    return _correction_constraint_ode_statistical(
+        ode_order=1,
+        linearise_fun=linearise_fun,
+        string_repr=f"<SLR0 with ode_order={1}>",
+    )
+
+
+def _correction_constraint_ode_statistical(
+    ode_order, linearise_fun, string_repr
+) -> _Correction:
+    def init(ssv, /):
         obs_like = impl.prototypes.observed()
         return ssv, obs_like
 
-    def estimate_error(self, hidden_state, _corr, /, vector_field, t):
+    def estimate_error(hidden_state, _corr, /, vector_field, t):
         f_wrapped = functools.partial(vector_field, t=t)
-        A, b = self.linearise(f_wrapped, hidden_state)
+        A, b = linearise_fun(f_wrapped, hidden_state)
         observed = impl.conditional.marginalise(hidden_state, (A, b))
 
         error_estimate = _estimate_error(observed)
         return error_estimate, observed, (A, b, f_wrapped)
 
-    def complete(self, hidden_state, corr, /):
+    def complete(hidden_state, corr, /):
         # Re-linearise (because the linearisation point changed)
         *_, f_wrapped = corr
-        A, b = self.linearise(f_wrapped, hidden_state)
+        A, b = linearise_fun(f_wrapped, hidden_state)
 
         # Condition
         observed, (_gain, corrected) = impl.conditional.revert(hidden_state, (A, b))
         return corrected, observed
 
-    def extract(self, hidden_state, _corr, /):
+    def extract(hidden_state, _corr, /):
         return hidden_state
+
+    return _Correction(
+        ode_order=ode_order,
+        name=string_repr,
+        init=init,
+        estimate_error=estimate_error,
+        complete=complete,
+        extract=extract,
+    )
 
 
 def _estimate_error(observed, /):
@@ -764,48 +799,6 @@ def _estimate_error(observed, /):
     output_scale = impl.stats.mahalanobis_norm_relative(zero_data, rv=observed)
     error_estimate_unscaled = np.squeeze(impl.stats.standard_deviation(observed))
     return output_scale * error_estimate_unscaled
-
-
-def correction_ts0(*, ode_order=1) -> _ODEConstraintTaylor:
-    """Zeroth-order Taylor linearisation."""
-    return _ODEConstraintTaylor(
-        ode_order=ode_order,
-        linearise_fun=impl.linearise.ode_taylor_0th(ode_order=ode_order),
-        string_repr=f"<TS0 with ode_order={ode_order}>",
-    )
-
-
-def correction_ts1(*, ode_order=1) -> _ODEConstraintTaylor:
-    """First-order Taylor linearisation."""
-    return _ODEConstraintTaylor(
-        ode_order=ode_order,
-        linearise_fun=impl.linearise.ode_taylor_1st(ode_order=ode_order),
-        string_repr=f"<TS1 with ode_order={ode_order}>",
-    )
-
-
-def correction_slr0(
-    cubature_fun=cubature_third_order_spherical,
-) -> _ODEConstraintStatistical:
-    """Zeroth-order statistical linear regression."""
-    linearise_fun = impl.linearise.ode_statistical_1st(cubature_fun)
-    return _ODEConstraintStatistical(
-        ode_order=1,
-        linearise_fun=linearise_fun,
-        string_repr=f"<SLR1 with ode_order={1}>",
-    )
-
-
-def correction_slr1(
-    cubature_fun=cubature_third_order_spherical,
-) -> _ODEConstraintStatistical:
-    """First-order statistical linear regression."""
-    linearise_fun = impl.linearise.ode_statistical_0th(cubature_fun)
-    return _ODEConstraintStatistical(
-        ode_order=1,
-        linearise_fun=linearise_fun,
-        string_repr=f"<SLR0 with ode_order={1}>",
-    )
 
 
 _T = TypeVar("_T")
