13c2f59f9a1ea62ec354ec9219641e14eea13dd3
Nicholas Krämer <nicokraemer@gmx.net>
2024-10-17 11:14:02 +0200

    Update the tests for the stats module

--- tests/test_solvers/test_stats/test_sample.py
+++ tests/test_solvers/test_stats/test_sample.py
@@ -10,15 +10,15 @@ from probdiffeq.impl import impl
 def fixture_approximation(ssm):
     vf, (u0,), (t0, t1) = ssm.default_ode
 
-    ibm = ivpsolvers.prior_ibm(num_derivatives=2)
+    output_scale = np.ones_like(impl.prototypes.output_scale())
+    tcoeffs = taylor.odejet_padded_scan(lambda y: vf(y, t=t0), (u0,), num=2)
+    ibm = ivpsolvers.prior_ibm(tcoeffs, output_scale)
     ts0 = ivpsolvers.correction_ts0()
     strategy = ivpsolvers.strategy_smoother(ibm, ts0)
     solver = ivpsolvers.solver(strategy)
     adaptive_solver = ivpsolve.adaptive(solver, atol=1e-2, rtol=1e-2)
 
-    output_scale = np.ones_like(impl.prototypes.output_scale())
-    tcoeffs = taylor.odejet_padded_scan(lambda y: vf(y, t=t0), (u0,), num=2)
-    init = solver.initial_condition(tcoeffs, output_scale)
+    init = solver.initial_condition()
     return ivpsolve.solve_adaptive_save_every_step(
         vf, init, t0=t0, t1=t1, adaptive_solver=adaptive_solver, dt0=0.1
     )
