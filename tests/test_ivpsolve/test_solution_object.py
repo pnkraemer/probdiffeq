34c2ae2af6039a048f4607a656586f0a01d6e25d
Nicholas Krämer <nicokraemer@gmx.net>
2024-10-17 16:15:17 +0200

    Update the solution-object tests

--- tests/test_ivpsolve/test_solution_object.py
+++ tests/test_ivpsolve/test_solution_object.py
@@ -1,29 +1,44 @@
 """Tests for interaction with the solution object."""
 
 from probdiffeq import ivpsolve, ivpsolvers, taylor
-from probdiffeq.backend import functools, testing
+from probdiffeq.backend import containers, functools, ode, testing
 from probdiffeq.backend import numpy as np
-from probdiffeq.impl import impl
+from probdiffeq.backend.typing import Array
+
+
+class Taylor(containers.NamedTuple):
+    """A non-standard Taylor-coefficient data structure."""
+
+    state: Array
+    velocity: Array
+    acceleration: Array
 
 
 @testing.fixture(name="approximate_solution")
-def fixture_approximate_solution(ssm):
-    vf, u0, (t0, t1) = ssm.default_ode
+@testing.parametrize("fact", ["dense", "blockdiag", "isotropic"])
+def fixture_approximate_solution(fact):
+    vf, u0, (t0, t1) = ode.ivp_lotka_volterra()
 
     # Generate a solver
-    output_scale = np.ones_like(impl.prototypes.output_scale())
-    tcoeffs = taylor.odejet_padded_scan(lambda y: vf(y, t=t0), u0, num=1)
-    ibm = ivpsolvers.prior_ibm(tcoeffs, output_scale=output_scale)
+    tcoeffs = Taylor(*taylor.odejet_padded_scan(lambda y: vf(y, t=t0), u0, num=2))
+    ibm, ssm = ivpsolvers.prior_ibm(tcoeffs, ssm_fact=fact)
 
-    ts0 = ivpsolvers.correction_ts0()
-    strategy = ivpsolvers.strategy_filter(ibm, ts0)
-    solver = ivpsolvers.solver_mle(strategy)
-    adaptive_solver = ivpsolve.adaptive(solver, atol=1e-2, rtol=1e-2)
+    ts0 = ivpsolvers.correction_ts0(ssm=ssm)
+    strategy = ivpsolvers.strategy_filter(ibm, ts0, ssm=ssm)
+    solver = ivpsolvers.solver_mle(strategy, ssm=ssm)
+    asolver = ivpsolve.adaptive(solver, atol=1e-2, rtol=1e-2, ssm=ssm)
 
     init = solver.initial_condition()
     return ivpsolve.solve_adaptive_save_every_step(
-        vf, init, t0=t0, t1=t1, dt0=0.1, adaptive_solver=adaptive_solver
+        vf, init, t0=t0, t1=t1, dt0=0.1, adaptive_solver=asolver, ssm=ssm
     )
+
+
+def test_u_inherits_data_structure(approximate_solution):
+    assert isinstance(approximate_solution.u, Taylor)
+
+    solution_t1 = approximate_solution[-1]
+    assert isinstance(solution_t1.u, Taylor)
 
 
 def test_getitem_possible_for_terminal_values(approximate_solution):
@@ -54,38 +69,46 @@ def test_iter_impossible(approximate_solution):
 
 
 @testing.fixture(name="approximate_solution_batched")
-def fixture_approximate_solution_batched(ssm):
-    vf, (u0,), (t0, t1) = ssm.default_ode
+@testing.parametrize("fact", ["dense", "blockdiag", "isotropic"])
+def fixture_approximate_solution_batched(fact):
+    vf, (u0,), (t0, t1) = ode.ivp_lotka_volterra()
 
     # Generate a solver
     save_at = np.linspace(t0, t1, endpoint=True, num=4)
 
-    @functools.vmap
     def solve(init):
         tcoeffs = (init, vf(init, t=None))
-        output_scale = np.ones_like(impl.prototypes.output_scale())
-        ibm = ivpsolvers.prior_ibm(tcoeffs, output_scale=output_scale)
+        ibm, ssm = ivpsolvers.prior_ibm(tcoeffs, ssm_fact=fact)
 
-        ts0 = ivpsolvers.correction_ts0()
-        strategy = ivpsolvers.strategy_filter(ibm, ts0)
-        solver = ivpsolvers.solver_mle(strategy)
-        adaptive_solver = ivpsolve.adaptive(solver, atol=1e-2, rtol=1e-2)
+        ts0 = ivpsolvers.correction_ts0(ssm=ssm)
+        strategy = ivpsolvers.strategy_filter(ibm, ts0, ssm=ssm)
+        solver = ivpsolvers.solver_mle(strategy, ssm=ssm)
+        adaptive_solver = ivpsolve.adaptive(solver, atol=1e-2, rtol=1e-2, ssm=ssm)
 
         initcond = solver.initial_condition()
         return ivpsolve.solve_adaptive_save_at(
-            vf, initcond, save_at=save_at, adaptive_solver=adaptive_solver, dt0=0.1
+            vf,
+            initcond,
+            save_at=save_at,
+            adaptive_solver=adaptive_solver,
+            dt0=0.1,
+            ssm=ssm,
         )
 
-    return solve(np.stack((u0, u0 + 0.1, u0 + 0.2)))
+    u0_batched = u0[None, ...]
+    solve = functools.vmap(solve)
+    return solve(u0_batched)
 
 
 def test_batched_getitem_possible(approximate_solution_batched):
     solution_type = type(approximate_solution_batched)
-    for idx in (0, 1, 2):
+    for idx in (0,):
         approximate_solution = approximate_solution_batched[idx]
         assert isinstance(approximate_solution, solution_type)
         assert np.allclose(approximate_solution.t, approximate_solution_batched.t[idx])
-        assert np.allclose(approximate_solution.u, approximate_solution_batched.u[idx])
+
+        for u1, u2 in zip(approximate_solution.u, approximate_solution_batched.u[idx]):
+            assert np.allclose(u1, u2)
 
 
 def test_batched_iter_possible(approximate_solution_batched):
@@ -93,17 +116,5 @@ def test_batched_iter_possible(approximate_solution_batched):
     for idx, approximate_solution in enumerate(approximate_solution_batched):
         assert isinstance(approximate_solution, solution_type)
         assert np.allclose(approximate_solution.t, approximate_solution_batched.t[idx])
-        assert np.allclose(approximate_solution.u, approximate_solution_batched.u[idx])
-
-
-def test_marginal_nth_derivative_of_solution(approximate_solution):
-    """Assert that each $n$th derivative matches the quantity of interest's shape."""
-    # Assert that the marginals have the same shape as the qoi.
-    for i in (0, 1):
-        marginals = approximate_solution.marginals
-        derivatives = impl.stats.marginal_nth_derivative(marginals, i)
-        assert derivatives.mean.shape == approximate_solution.u.shape
-
-    # if the requested derivative is not in the state-space model, raise a ValueError
-    with testing.raises(ValueError):
-        _ = impl.stats.marginal_nth_derivative(approximate_solution.marginals, 100)
+        for u1, u2 in zip(approximate_solution.u, approximate_solution_batched.u[idx]):
+            assert np.allclose(u1, u2)
