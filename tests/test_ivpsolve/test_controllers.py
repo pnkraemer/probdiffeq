19f5d113786d59cd38168e5df77d3af34d4ca212
Nicholas Krämer <nicokraemer@gmx.net>
2024-10-25 18:16:07 +0200

    Apply the controller repeatedly to tighten the test

--- tests/test_ivpsolve/test_controllers.py
+++ tests/test_ivpsolve/test_controllers.py
@@ -4,17 +4,19 @@ from probdiffeq import ivpsolve
 from probdiffeq.backend import numpy as np
 
 
-def test_equivalence_pi_vs_i(dt=0.1428, norm=3.142, rate=3):
+def test_equivalence_pi_vs_i(dt=0.1428, norm=3.142, rate=3, num_applies=4):
     ctrl_pi = ivpsolve.control_proportional_integral(
         power_integral_unscaled=1.0, power_proportional_unscaled=0.0
     )
     ctrl_i = ivpsolve.control_integral()
 
     x_pi = ctrl_pi.init(dt)
-    x_pi = ctrl_pi.apply(x_pi, norm, rate)
+    for _ in range(num_applies):
+        x_pi = ctrl_pi.apply(x_pi, norm, rate)
     x_pi = ctrl_pi.extract(x_pi)
 
     x_i = ctrl_i.init(dt)
-    x_i = ctrl_i.apply(x_i, norm, rate)
+    for _ in range(num_applies):
+        x_i = ctrl_i.apply(x_i, norm, rate)
     x_i = ctrl_i.extract(x_i)
     assert np.allclose(x_i, x_pi)
