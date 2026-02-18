5f4287cca67dc0d717668533a1ecc6b78d8d1a5d
Nicholas Krämer <nicokraemer@gmx.net>
2026-02-18 09:25:44 +0100

    Avoid testing the product space of configs in test_adaptive_save_at to save minutes of test time

--- tests/test_ivpsolve/test_adaptive_save_at.py
+++ tests/test_ivpsolve/test_adaptive_save_at.py
@@ -1,7 +1,8 @@
 """Assert that the base adaptive solver is accurate."""
 
 from probdiffeq import ivpsolve, probdiffeq, taylor
-from probdiffeq.backend import func, np, ode, testing, tree
+from probdiffeq.backend import func, np, ode, structs, testing, tree
+from probdiffeq.backend.typing import Callable
 
 
 @testing.fixture(name="ivp")
@@ -9,62 +10,86 @@ def ivp_lotka_volterra():
     return ode.ivp_lotka_volterra()
 
 
-@testing.case
-def case_strategy_filter():
-    return probdiffeq.strategy_filter
+@structs.dataclass
+class Factory:
+    """A solver factory.
+
+    This data structure ensures that we don't test
+    the product space of all configurations, whose size
+    grows too quickly.
+    """
+
+    strategy: Callable = probdiffeq.strategy_filter
+    solver: Callable = probdiffeq.solver
+    constraint: Callable = probdiffeq.constraint_ode_ts0
+    errorest: Callable = probdiffeq.errorest_local_residual_std
 
 
 @testing.case
-def case_strategy_smoother_fixedpoint():
-    return probdiffeq.strategy_smoother_fixedpoint
+def case_factory_strategy_filter():
+    return Factory(strategy=probdiffeq.strategy_filter)
 
 
 @testing.case
-def case_solver_solver():
-    return probdiffeq.solver
+def case_factory_strategy_smoother_fixedpoint():
+    return Factory(strategy=probdiffeq.strategy_smoother_fixedpoint)
 
 
 @testing.case
-def case_solver_mle():
-    return probdiffeq.solver_mle
+def case_factory_solver_solver():
+    return Factory(solver=probdiffeq.solver)
 
 
 @testing.case
-def case_solver_dynamic_without_relinearization():
-    return func.partial(probdiffeq.solver_dynamic, re_linearize_after_calibration=False)
+def case_factory_solver_mle():
+    return Factory(solver=probdiffeq.solver_mle)
 
 
 @testing.case
-def case_solver_dynamic_with_relinearization():
-    return func.partial(probdiffeq.solver_dynamic, re_linearize_after_calibration=True)
+def case_factory_solver_dynamic_without_relinearization():
+    dynamic = func.partial(
+        probdiffeq.solver_dynamic, re_linearize_after_calibration=False
+    )
+    return Factory(solver=dynamic)
 
 
 @testing.case
-def case_constraint_ode_ts0():
-    return probdiffeq.constraint_ode_ts0
+def case_factory_solver_dynamic_with_relinearization():
+    dynamic = func.partial(
+        probdiffeq.solver_dynamic, re_linearize_after_calibration=True
+    )
+    return Factory(solver=dynamic)
+
+
+@testing.case
+def case_factory_constraint_ode_ts0():
+    return Factory(constraint=probdiffeq.constraint_ode_ts0)
 
 
 @testing.case
 @testing.parametrize("jacfun", [func.jacfwd, func.jacrev])
-def case_constraint_ode_ts1_materialize(jacfun):
+def case_factory_constraint_ode_ts1_materialize(jacfun):
     jacobian = probdiffeq.jacobian_materialize(jacfun=jacfun)
-    return func.partial(probdiffeq.constraint_ode_ts1, jacobian=jacobian)
+    constraint = func.partial(probdiffeq.constraint_ode_ts1, jacobian=jacobian)
+    return Factory(constraint=constraint)
 
 
 @testing.case
-def case_constraint_ode_ts1_hutchinson_fwd():
+def case_factory_constraint_ode_ts1_hutchinson_fwd():
     jacobian = probdiffeq.jacobian_hutchinson_fwd()
-    return func.partial(probdiffeq.constraint_ode_ts1, jacobian=jacobian)
+    constraint = func.partial(probdiffeq.constraint_ode_ts1, jacobian=jacobian)
+    return Factory(constraint=constraint)
 
 
 @testing.case
-def case_constraint_ode_ts1_hutchinson_rev():
+def case_factory_constraint_ode_ts1_hutchinson_rev():
     jacobian = probdiffeq.jacobian_hutchinson_rev()
-    return func.partial(probdiffeq.constraint_ode_ts1, jacobian=jacobian)
+    constraint = func.partial(probdiffeq.constraint_ode_ts1, jacobian=jacobian)
+    return Factory(constraint=constraint)
 
 
 @testing.case
-def case_constraint_root_ts1(ivp):
+def case_factory_constraint_root_ts1(ivp):
     vf, _u0, (_t0, _t1) = ivp
 
     jacobian = probdiffeq.jacobian_materialize()
@@ -83,11 +108,11 @@ def case_constraint_root_ts1(ivp):
             reason += ", likely due to the selected state-space factorisation."
             testing.skip(reason)
 
-    return constraint
+    return Factory(constraint=constraint)
 
 
 @testing.case
-def case_constraint_ode_slr0():
+def case_factory_constraint_ode_slr0():
     def constraint(*args, **kwargs):
         try:
             return probdiffeq.constraint_ode_slr0(*args, **kwargs)
@@ -96,11 +121,11 @@ def case_constraint_ode_slr0():
             reason += ", likely due to the selected state-space factorisation."
             testing.skip(reason)
 
-    return constraint
+    return Factory(constraint=constraint)
 
 
 @testing.case
-def case_constraint_ode_slr1():
+def case_factory_constraint_ode_slr1():
     def constraint(*args, **kwargs):
         try:
             return probdiffeq.constraint_ode_slr1(*args, **kwargs)
@@ -109,42 +134,39 @@ def case_constraint_ode_slr1():
             reason += ", likely due to the selected state-space factorisation."
             testing.skip(reason)
 
-    return constraint
+    return Factory(constraint=constraint)
 
 
 @testing.case
-def case_errorest_local_residual_cached():
-    return func.partial(
+def case_factory_errorest_local_residual_cached():
+    errorest = func.partial(
         probdiffeq.errorest_local_residual_std, re_linearize_before_errorest=True
     )
+    return Factory(errorest=errorest)
 
 
 @testing.case
-def case_errorest_local_residual():
-    return func.partial(
+def case_factory_errorest_local_residual():
+    errorest = func.partial(
         probdiffeq.errorest_local_residual_std, re_linearize_before_errorest=False
     )
+    return Factory(errorest=errorest)
 
 
-@testing.parametrize("fact", ["dense", "isotropic", "blockdiag"])
-@testing.parametrize_with_cases("strategy_factory", ".", prefix="case_strategy_")
-@testing.parametrize_with_cases("solver_factory", ".", prefix="case_solver_")
-@testing.parametrize_with_cases("constraint_factory", ".", prefix="case_constraint_")
-@testing.parametrize_with_cases("errorest_factory", ".", prefix="case_errorest_")
-def test_output_matches_reference(
-    ivp, fact, solver_factory, constraint_factory, strategy_factory, errorest_factory
-):
+@testing.parametrize("ssm_fact", ["dense", "isotropic", "blockdiag"])
+@testing.parametrize_with_cases("factory", ".", prefix="case_factory_")
+def test_output_matches_reference(ivp, ssm_fact, factory: Factory):
     vf, u0, (t0, t1) = ivp
 
     # Build a solver
     tcoeffs = taylor.odejet_padded_scan(lambda y: vf(y, t=t0), u0, num=4)
-    init, iwp, ssm = probdiffeq.prior_wiener_integrated(tcoeffs, ssm_fact=fact)
-    strategy = strategy_factory(ssm=ssm)
-    constraint = constraint_factory(vf, ssm=ssm)
-    solver = solver_factory(
+    init, iwp, ssm = probdiffeq.prior_wiener_integrated(tcoeffs, ssm_fact=ssm_fact)
+    strategy = factory.strategy(ssm=ssm)
+    constraint = factory.constraint(vf, ssm=ssm)
+    solver = factory.solver(
         strategy=strategy, prior=iwp, constraint=constraint, ssm=ssm
     )
-    errorest = errorest_factory(prior=iwp, ssm=ssm, constraint=constraint)
+    errorest = factory.errorest(prior=iwp, ssm=ssm, constraint=constraint)
 
     # Compute the PN solution
     save_at = np.linspace(t0, t1, endpoint=True, num=7)
