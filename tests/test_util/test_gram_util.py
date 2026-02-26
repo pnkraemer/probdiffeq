76df0a33fde036a6fe0e8cdc95d09612697de016
Nicholas Krämer <nicokraemer@gmx.net>
2026-02-26 12:26:43 +0100

    Move gram utilities to probdiffeq.util.gram_util

---
+++ tests/test_util/test_gram_util.py
@@ -0,0 +1,55 @@
+from probdiffeq.backend import func, linalg, np, random, testing
+from probdiffeq.util import gram_util
+
+
+def case_method_pade_legendre_13():
+    return gram_util.pade_and_legendre_13()
+
+
+def case_method_pade_legendre_9():
+    return gram_util.pade_and_legendre_9()
+
+
+def case_method_pade_legendre_7():
+    return gram_util.pade_and_legendre_7()
+
+
+def case_method_pade_legendre_5():
+    return gram_util.pade_and_legendre_5()
+
+
+def case_method_pade_legendre_3():
+    return gram_util.pade_and_legendre_3()
+
+
+@testing.parametrize("seed", [1])
+@testing.parametrize("nrows", [8], ids=["size8"])
+@testing.parametrize("ncols", [2, 8], ids=["tall", "square"])
+@testing.parametrize("use_triu", [True, False], ids=["triu", "dense"])
+@testing.parametrize_with_cases("pade_legendre", ".", prefix="case_method_")
+def test_exact(seed, nrows, ncols, pade_legendre, use_triu):
+
+    # Define the problem
+    key = random.prng_key(seed=seed)
+    A = 0.01 * random.normal(key, shape=(nrows, nrows))
+    B = random.normal(key, shape=(nrows, ncols))
+
+    if use_triu:
+        A = np.triu(A)
+        solve = linalg.solve_triangular
+    else:
+        solve = linalg.solve_lu
+
+    # Evaluate a baseline
+    algorithm = gram_util.exp_gram_matrix_fraction()
+    eA1, G1 = func.jit(algorithm)(A, B)
+
+    # Run our algorithm
+    algorithm = gram_util.exp_gram_cholesky(pade_legendre=pade_legendre, solve=solve)
+    eA2, L2 = func.jit(algorithm)(A, B)
+
+    # Choose a tolerance based on the dtype.
+    tol = np.sqrt(np.finfo_eps(G1.dtype))
+    assert testing.allclose(eA1, eA2, atol=tol, rtol=tol)
+    assert testing.allclose(G1, L2 @ L2.T, atol=tol, rtol=tol)
+    assert testing.allclose(L2, np.tril(L2), atol=tol, rtol=tol)
