da5b67d59e475a9c00df5da766b2d748911d1153
Nicholas Krämer <nicokraemer@gmx.net>
2026-06-12 08:51:31 +0200

    Improve blackjax example plot

--- examples/E1_parameter_estimation_blackjax.py
+++ examples/E1_parameter_estimation_blackjax.py
@@ -5,7 +5,7 @@ initial value problems (IVPs) using Markov Chain Monte Carlo (MCMC)
 methods as provided by [BlackJAX](https://blackjax-devs.github.io/blackjax/).
 """
 
-import functools
+from collections.abc import Callable
 
 import blackjax
 import jax
@@ -33,13 +33,18 @@ def main():
         del t
         return f(y, *f_args)
 
-    # Construct solvers
-    solve = solve_fixed(vf, t0=t0, t1=t1, num=200)
+    # Construct solvers. Use fixed steps
+    # because window adaptation runs maaaany iterations
+    # and values and gradients of fixed-step solvers are
+    # faster than for adaptive solvers (if the ODE allows fixed steps)
+    solve = solve_fixed(vf, t0=t0, t1=t1, num=250)
 
     # Determine true parameters and create data
+    key = jax.random.PRNGKey(0)
+    std = 0.1
     theta_true = u0 + 0.5 * jnp.flip(u0)
     solution_true = solve(theta_true)
-    data = solution_true.u.mean[0]
+    data = solution_true.u.mean[0] + std * jax.random.normal(key, (2,))
 
     # Determine initial guesses
     theta0 = u0
@@ -48,18 +53,17 @@ def main():
     # Choose a Gaussian prior centered at the initial guess with a large variance.
     mean = theta0
     cov = jnp.eye(2) * 30
-    log_M = log_posterior(solve=solve, data=data, mean=mean, cov=cov)
+    log_M = log_posterior(solve=solve, data=data, mean=mean, cov=cov, std=std)
     log_M(theta0)
 
     # From here on, BlackJAX takes over:
 
     # Warmup
     print("\nRunning window adaptation...", end="")
-    key = jax.random.PRNGKey(0)
     key, subkey = jax.random.split(key, num=2)
     warmup = blackjax.window_adaptation(blackjax.nuts, log_M, progress_bar=False)
-    warmup_results, _ = warmup.run(subkey, theta0, num_steps=200)
-    print("done.")
+    warmup_results, _ = warmup.run(subkey, theta0, num_steps=150)
+    print(" done.")
 
     # Inference loop
     print("\nRunning inference loop...", end="")
@@ -68,47 +72,46 @@ def main():
         step_size=warmup_results.parameters["step_size"],
         inverse_mass_matrix=warmup_results.parameters["inverse_mass_matrix"],
     )
+    loop = inference_loop(kernel=nuts_kernel, num_samples=200)
     key, subkey = jax.random.split(key, num=2)
-    states = inference_loop(
-        subkey, kernel=nuts_kernel, initial_state=warmup_results.state, num_samples=150
-    )
-    samples = states.position
-    print("done.")
+    samples = loop(subkey, initial_state=warmup_results.state)
+    print(" done.")
 
     # Create the plot
-    _fig, ax = plt.subplot_mosaic(
-        [["smp", "dens"]], sharex=True, sharey=True, figsize=(8, 3)
-    )
+    mosaic = [["smp", "dens"]]
+    _fig, ax = plt.subplot_mosaic(mosaic, sharex=True, sharey=True, figsize=(8, 3))
 
     # Plot the samples
-    ax["smp"].set_title("Posterior samples (parameter space)", fontsize="medium")
-    ax["smp"].plot(samples[:, 0], samples[:, 1], ".", alpha=0.5, markersize=4)
-    ax["smp"].plot(theta_true[0], theta_true[1], "P", label="Truth", markersize=8)
-    ax["smp"].plot(theta0[0], theta0[1], "P", label="Initial guess", markersize=8)
-    ax["smp"].legend()
+    ax["smp"].set_title("Posterior samples", fontsize="medium")
+    ax["smp"].plot(samples[:, 0], samples[:, 1], ".", color="dimgrey")
+    ax["smp"].plot(theta0[0], theta0[1], "P", color="C0", label="Initial guess")
+    ax["smp"].plot(theta_true[0], theta_true[1], "X", color="C1", label="Truth")
+    ax["smp"].legend(fontsize="small")
 
     # Create a meshgrid for plotting the density
-    xlim = 17, jnp.amax(samples[:, 0]) + 0.5
-    ylim = 17, jnp.amax(samples[:, 1]) + 0.5
-    xs = jnp.linspace(*xlim, endpoint=True, num=200)
-    ys = jnp.linspace(*ylim, endpoint=True, num=200)
+    xmin = jnp.minimum(jnp.amin(samples[:, 0]), theta0[0]) - 1.5
+    xmax = jnp.maximum(jnp.amax(samples[:, 0]), theta0[0]) + 1.5
+    ymin = jnp.minimum(jnp.amin(samples[:, 1]), theta0[1]) - 1.5
+    ymax = jnp.maximum(jnp.amax(samples[:, 1]), theta0[1]) + 1.5
+    xs = jnp.linspace(xmin, xmax, endpoint=True, num=200)
+    ys = jnp.linspace(ymin, ymax, endpoint=True, num=200)
     Xs, Ys = jnp.meshgrid(xs, ys)
 
     # Evaluate the density
     Thetas = jnp.stack((Xs, Ys))
     log_M_vmapped_x = jax.vmap(log_M, in_axes=-1, out_axes=-1)
     log_M_vmapped = jax.vmap(log_M_vmapped_x, in_axes=-1, out_axes=-1)
-    Zs = log_M_vmapped(Thetas)
+    Zs = jax.jit(log_M_vmapped)(Thetas)
 
     # Plot the density
     ax["dens"].set_title("Target density", fontsize="medium")
-    im = ax["dens"].pcolormesh(Xs, Ys, jnp.exp(Zs), cmap="cividis", alpha=0.8)
+    im = ax["dens"].pcolormesh(Xs, Ys, jnp.exp(Zs), cmap="cividis")
     plt.colorbar(im)
     plt.show()
 
 
-def solve_fixed(vf, *, t0, t1, num):
-    """Create an adaptive solver (for visualisation)."""
+def solve_fixed(vf, *, t0, t1, num) -> Callable:
+    """Create a solver."""
     ssm = probdiffeq.state_space_model_isotropic()
     ts0 = ssm.constraint_ode_ts0(vf)
     strategy = probdiffeq.strategy_filter()
@@ -119,9 +122,7 @@ def solve_fixed(vf, *, t0, t1, num):
     @jax.jit
     def solve(theta, /):
         """Evaluate the parameter-to-solution map, solving on an adaptive grid."""
-        jetexpand = probdiffeq.jetexpand_ode_padded_scan(num=3)
-        tcoeffs, _ = jetexpand(vf, (theta,), t=t0)
-
+        tcoeffs = [theta, vf(theta, t=t0)]
         iwp = ssm.prior_wiener_integrated(tcoeffs)
         sol = solve_fn(iwp, grid=grid)
         return jax.tree.map(lambda s: s[-1], sol)
@@ -129,7 +130,7 @@ def solve_fixed(vf, *, t0, t1, num):
     return solve
 
 
-def log_posterior(*, solve, data, mean, cov, obs_std=0.1):
+def log_posterior(*, solve, data, mean, cov, std) -> Callable:
     """Create a log-posterior density function."""
     loss = probdiffeq.loss_lml_terminal_values()
     logpdf_normal = jax.scipy.stats.multivariate_normal.logpdf
@@ -138,25 +139,28 @@ def log_posterior(*, solve, data, mean, cov, obs_std=0.1):
     def logposterior(theta, /):
         """Evaluate the logposterior-function of the data."""
         solution = solve(theta)
-        logpdf_data = loss(data, std=obs_std, marginals=solution.u)
+        logpdf_data = loss(data, std=std, marginals=solution.u)
         logpdf_prior = logpdf_normal(theta, mean=mean, cov=cov)
         return logpdf_data + logpdf_prior
 
     return logposterior
 
 
-@functools.partial(jax.jit, static_argnames=["kernel", "num_samples"])
-def inference_loop(rng_key, kernel, initial_state, num_samples):
+def inference_loop(*, kernel, num_samples) -> Callable:
     """Run BlackJAX' inference loop."""
 
-    def one_step(state, rng_key):
-        state, _ = kernel.step(rng_key, state)
-        return state, state
+    @jax.jit
+    def loop(rng_key, initial_state):
+        def one_step(state, rng_key):
+            state, _ = kernel.step(rng_key, state)
+            return state, state
 
-    keys = jax.random.split(rng_key, num_samples)
-    _, states = jax.lax.scan(one_step, initial_state, keys)
+        keys = jax.random.split(rng_key, num_samples)
+        _, states = jax.lax.scan(one_step, initial_state, keys)
 
-    return states
+        return states.position
+
+    return loop
 
 
 if __name__ == "__main__":
